3439c4e7e6dece367ddccacfb0d71661c8470897
Dan McGlinn <danmcglinn@gmail.com>
2011-12-14 18:49:19 +0000

    Added additional functions and graphical exploration of the probability models for the spatial abundance distribution pi. I am still unsure about the fomulation of the iterative mete solution, however it does appear equivalent to HEAP which Harte suggests is the case in his book.

--- spat_anayltical_prob_funcs.R
+++ spat_anayltical_prob_funcs.R
@@ -1,4 +1,8 @@
 
+piBin = function(n,A,no,Ao){
+  p = A/Ao
+  (factorial(no)/(factorial(n)*factorial(no-n))) * p^n * (1 - p)^(no - n)
+}
 
 g = function(no,Mo){
   ## Harte book Eq. 4.10 pg. 93
@@ -19,10 +23,17 @@ piLap = function(n,A,no,Ao){
 piHEAP = function(n,A,no,Ao){
   ## Harte book Eq. 4.15 pg. 94
   i = log2(Ao/A)
-  sum(sapply(n:no,function(q) piLap(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+  if(length(n) > 1){
+    sapply(1:length(n),function(j){
+      sum(sapply(n[j]:no, function(q) piLap(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+    })
+  }  
+  else{
+    sum(sapply(n:no,function(q) piLap(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+  }
 }
 
-piNegBi = function(n,A,no,Ao,k){
+piNegBi = function(n,A,no,Ao,k=1){
   ## Harte book Eq. 4.16 pg. 95
   nbar = no*A/Ao
   (factorial(n + k - 1) / (factorial(n)*factorial(k-1))) * (nbar / (k + nbar))^n * (k / (k+nbar))^k
@@ -42,13 +53,71 @@ piMETE = function(n,A,no,Ao){
   }
 }
 
+piMETEiter = function(n,A,no,Ao){
+  ## iterative METE approach, downscaling only
+  ## special case when A = Ao / 2 applies
+  ## Harge Book Eq. 7.51 pg. 159
+  i = log2(Ao/A)
+  if(length(n) > 1){
+    sapply(1:length(n),function(j){
+      sum(sapply(n[j]:no, function(q) piMETE(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+    })
+  }  
+  else{
+    sum(sapply(n:no,function(q) piMETE(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+  }
+}
+
 ## generate some comparitive frequency distributions
 ## example from pg 96 fig 4.1
-no = 112
+setwd('c:/users/white lab/documents/lab data/maxent/spat')
+pdf('analytical_prob_funcs.pdf',width=14,height=7)
+par(mfrow=c(1,2))
+no = 100
 n = 0:5
 Ao = 64
 A = 1
+out = matrix(NA,nrow=6,ncol=length(n))
+out[1,] = piBin(n,A,no,Ao)
+out[2,] = piLap(n,A,no,Ao)
+out[3,] = piHEAP(n,A,no,Ao)
+out[4,]= piMETE(n,A,no,Ao)
+out[5,]= piMETEiter(n,A,no,Ao)
+out[6,] = piNegBi(n,A,no,Ao)
 
-lap = piLap(n,A,no,Ao)
 
-  
+plot(n,out[1,],ylim=range(out,na.rm=TRUE),type='n',ylab='Probabiliy',
+     main='No = 100, A = Ao/64')
+for(i in 1:4)
+  lines(n,out[i,],col=i,type='l',lwd=2)
+for(i in 5:6)
+ points(n,out[i,],col=i,pch=19,cex=1.5)
+legend('topright',c('bin','lap','heap','mete','meteiter','negbin(k=1)'),col=1:6,
+       lwd=c(rep(3,4),NA,NA),lty=c(rep(1,4),NA,NA),pch=c(rep(NA,4),19,19),cex=2,bty='n')
+## lap, mete and negbi are equivalent
+## heap and meteiter are equivalent
+
+no = 5
+n = 0:5
+Ao = 4
+A = 1
+
+out = matrix(NA,nrow=6,ncol=length(n))
+out[1,] = piBin(n,A,no,Ao)
+out[2,] = piLap(n,A,no,Ao)
+out[3,] = piHEAP(n,A,no,Ao)
+out[4,]= piMETE(n,A,no,Ao)
+out[5,]= piMETEiter(n,A,no,Ao)
+out[6,] = piNegBi(n,A,no,Ao)
+
+plot(n,out[1,],ylim=range(out,na.rm=TRUE),type='n',ylab='Probability',
+     main='No = 5, A = Ao/4')
+for(i in 1:4)
+  lines(n,out[i,],col=i,type='l',lwd=2)
+for(i in 5:6)
+ points(n,out[i,],col=i,pch=19,cex=1.5)
+legend('topright',c('bin','lap','heap','mete','meteiter','negbin(k=1)'),col=1:6,
+       lwd=c(rep(3,4),NA,NA),lty=c(rep(1,4),NA,NA),pch=c(rep(NA,4),19,19),cex=2,bty='n')
+## mete and negbi are equivalent
+## heap and meteiter are equivalent
+dev.off()
