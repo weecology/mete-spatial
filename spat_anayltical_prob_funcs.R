726eace1d5ad00f886b42949a79ba9d71b180d6b
Dan McGlinn <danmcglinn@gmail.com>
2011-12-14 01:00:45 +0000

    This script contains different analytical formulations for how to calculate the probability that n indviduals will be present in a single cell of area A given no individuals in a total area Ao.

---
+++ spat_anayltical_prob_funcs.R
@@ -0,0 +1,54 @@
+
+
+g = function(no,Mo){
+  ## Harte book Eq. 4.10 pg. 93
+  ## returns the number of ways no indistinguishable individuals can be 
+  ## arranged into Mo cells
+  factorial(Mo + no - 1) / (factorial(no) * factorial(Mo - 1))
+}
+
+piLap = function(n,A,no,Ao){
+  ## Harte book Eq. 4.11 pg. 93
+  ## returns the probability that n individuals are located in a randomly
+  ## chosen cell that is Mo times smaller than the area it is embedded within.
+  ## no is the total number of individuals in the larger area.
+  Mo = Ao / A
+  g(no - n, Mo - 1) / g(no, Mo)
+}
+
+piHEAP = function(n,A,no,Ao){
+  ## Harte book Eq. 4.15 pg. 94
+  i = log2(Ao/A)
+  sum(sapply(n:no,function(q) piLap(q,Ao/2^(i-1),no,Ao) / (q + 1)))
+}
+
+piNegBi = function(n,A,no,Ao,k){
+  ## Harte book Eq. 4.16 pg. 95
+  nbar = no*A/Ao
+  (factorial(n + k - 1) / (factorial(n)*factorial(k-1))) * (nbar / (k + nbar))^n * (k / (k+nbar))^k
+}
+
+piMETE = function(n,A,no,Ao){
+  ## special case when A = Ao / 2
+  ## Harge Book Eq. 7.51 pg. 159
+  ## approximation of pi when A << Ao
+  ## Harte book Eq. 7.53 pg. 160
+  if(Ao / A == 2){
+    1 / (1 + no) 
+  }
+  else{
+    nbar = no*A/Ao
+    (nbar/(1+nbar))^n / (1+nbar)
+  }
+}
+
+## generate some comparitive frequency distributions
+## example from pg 96 fig 4.1
+no = 112
+n = 0:5
+Ao = 64
+A = 1
+
+lap = piLap(n,A,no,Ao)
+
+  
