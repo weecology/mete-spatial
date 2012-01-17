e659152eef265d475cbd7239de955232f6ebb042
Dan McGlinn <danmcglinn@gmail.com>
2012-01-17 20:44:53 +0000

    This script now simply imports the Serpentine community matrix rather than generating it on the fly

--- spat_serp_sad.R
+++ spat_serp_sad.R
@@ -1,14 +1,7 @@
 setwd('/home/danmcglinn/maxent/spat')
 
-source('spat_sim_vario_func.R')
-
-dat = read.csv('./data/serpentine_data.csv',header=T)
-dat = dat[-(nrow(dat):(nrow(dat)-2)),]
-dat[1,] = ifelse(is.na(dat[1,]),0,dat[1,])
-x = rep(1:16,each=16)
-y = rep(1:16,times=16)
-grains = c(1,4,16)
-comms = quadAggregator(mat = dat, coord = cbind(x,y), grains = grains)
+comms = read.csv('./data/serp_comms.csv')
+grains = unique(comms$grain)
 
 ## calculate empirical SAD/RAD
 sadAbs = apply(comms[comms[,1]==grains[1],-(1:3)],2,sum)
