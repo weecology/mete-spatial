5d6dca8fd67db1727b48334c9e63c45aa3ae4262
Dan McGlinn <danmcglinn@gmail.com>
2012-01-21 08:07:21 +0000

    Added function piHEAPfast which uses compiled C code to much more quickly calculate Harte's HEAP model

--- spat_analytical_prob_funcs.R
+++ spat_analytical_prob_funcs.R
@@ -44,6 +44,8 @@ piLap = function(n,A,no,Ao){
 }
 
 piHEAP = function(n,A,no,Ao){
+  ##HEAP model
+  ##Harte book Eq. 4.16 pg. 93
   i = log2(Ao/A)
   if(i == 1)
     out = 1/(no+1)
@@ -51,6 +53,24 @@ piHEAP = function(n,A,no,Ao){
     A = A*2 
     out = sum(sapply(n:no,function(q) piHEAP(q,A,no,Ao) / (q + 1)))
   }
+  return(out)
+}
+
+piHEAPfast = function(n,A,no,Ao,OS='linux'){
+  ##HEAP model 
+  ##Harte book Eq. 4.16 pg. 93
+  ##The source code is in the file HEAP.c
+  if(OS == 'linux'){
+    if(!is.loaded('heap.so'))
+      dyn.load('heap.so')
+  }  
+  else{
+    if(!is.loaded('heap.dll'))
+      dyn.load('heap.dll')
+  }  
+  out = sapply(n,function(x){
+        .C("HEAP",n=as.integer(x),A=as.integer(A),no=as.integer(no),
+           Ao=as.integer(Ao),prob=as.double(0))$prob})
   return(out)
 }
 
