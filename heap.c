760485ad2a9054b24d122ca23f7e0b6677388157
Dan McGlinn <danmcglinn@gmail.com>
2012-01-21 07:49:03 +0000

    This C script computes Harte's HEAP model, Eq. 4.15 in Harte's book.

---
+++ heap.c
@@ -0,0 +1,34 @@
+/*
+Purpose: To compute the HEAP model, Eq. 4.15 in Harte's book
+Arguments:
+n -- the number of individuals
+A -- the area
+no -- the total number of individuals in the pool
+Ao -- the total area of the pool
+prob -- the probability of observing n individuals (what we want back)
+
+*/
+
+#include <R.h>
+#include <math.h> 
+
+void HEAP(int *n, int *A, int *no, int *Ao, double *prob){
+    double rHEAP(int n, int A, int no, int Ao) ;
+    *prob = rHEAP(*n,*A,*no,*Ao) ;
+} 
+
+double rHEAP(int n, int A, int no, int Ao){
+    int i, q ;  
+    double total = 0;
+    i = log2(Ao / A) ; 
+    if(i == 1){
+        return 1.0 / (no + 1) ; 
+    }
+    else{
+        A = A * 2 ;
+        for (q = n ; q < (no + 1) ; q++){
+            total += rHEAP(q,A,no,Ao) / (q + 1) ;
+        }
+        return total ; 
+    }
+}
