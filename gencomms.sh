39d3e8fb1293876adb81460b2fde11423fc64183
Dan McGlinn <danmcglinn@gmail.com>
2012-02-01 00:45:11 +0000

    Shell script for generating the HEAP communities over a range of S and N.

---
+++ gencomms.sh
@@ -0,0 +1,21 @@
+for S in 10  11  13  14  16  18  21  23  26  30  34  38  43  48  55  62  70  78  89 100
+
+
+
+do
+ 
+ 
+  for N in 120 186 289 447 694 1076 1668 2587 4011 6220 9646 14957 23193 35965 55769 86479 134099 207941 322444 500000
+
+
+  do
+
+
+    python spat_community_generation.py $S $N 200 13 False None space &
+
+    sleep 20
+
+  done
+done
+
+
