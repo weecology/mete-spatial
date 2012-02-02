88d24e2b366bf08714aad61cdc5797b0c5ac83f6
Ethan White <ethan@weecology.org>
2012-02-02 21:40:09 +0000

    Moving functions from spat.py into spat_community_generation.py

--- spat_community_generation.py
+++ spat_community_generation.py
@@ -19,7 +19,63 @@ import sys
 import os
 
 import mete
-import spat
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
+
+def comm_filename(S, N, ncomm, bisec, transect = False, abu = None,
+                   comm_name = None):
+    """Creates the file name of a simulated community produced by the function
+    sim_spatial_whole() in the mete.py module
+    
+    Keyword arguments:
+    S -- the number of species
+    N -- the number of individuals
+    ncomm -- the number of communities that were generated
+    bisec -- the number of bisections
+    transect -- a boolean if False then it is assumed a 2D grid was generated
+    abu -- the path to an abundance file else this should be None
+    shrt_name -- the short hand name for the output community, this overrides
+                 S and N in the output file name.
+    """
+                 
+    if(transect):
+        if(comm_name is None):  
+            if(abu is None):
+                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
+                            str(N) + '_C' + str(ncomm) + '_B' + str(bisec) + 
+                            '_transect.txt')
+            else:
+                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
+                            str(N) + '_empirSAD' + '_C' +  str(ncomm) + '_B' +
+                            str(bisec) + '_transect.txt')
+        else:
+            if(abu is None):
+                filename = ('./comms/simulated_comms_' + comm_name + '_C' +
+                            str(ncomm) + '_B' + str(bisec) + '_transect.txt')
+            else:
+                filename = ('./comms/simulated_comms_' + comm_name + '_empirSAD'
+                            + '_C' + str(ncomm) + '_B' + str(bisec) + 
+                            '_transect.txt')
+    else:
+        if(comm_name is None):
+            if(abu is None):
+                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
+                            str(N) + '_C' + str(ncomm) + '_B' + str(bisec) +
+                            '_grid.txt')
+            else:
+                filename = ('./comms/simulated_comms_S' + str(S) + '_N' + 
+                            str(N) + '_empirSAD' + '_C' + str(ncomm) + '_B' +
+                            str(bisec) + '_grid.txt')
+        else:
+            if(abu is None):
+                filename = ('./comms/simulated_comms_' + comm_name + '_C' +
+                            str(ncomm) + '_B' + str(bisec) + '_grid.txt')
+            else:
+                filename = ('./comms/simulated_comms_' + comm_name + '_empirSAD'
+                            + '_C' + str(ncomm) + '_B' + str(bisec) + '_grid.txt')
+    return(filename)
+
 
 if 'comms' not in os.listdir(os.path.curdir):
     os.mkdir('comms')
@@ -29,7 +85,7 @@ if(len(sys.argv) > 1):
     N = int(sys.argv[2]) 
     ncomm = int(sys.argv[3]) 
     bisec = int(sys.argv[4])
-    transect = spat.str2bool(sys.argv[5])
+    transect = str2bool(sys.argv[5])
     abu = sys.argv[6]
     shrt_name = sys.argv[7]
 else:
@@ -68,7 +124,7 @@ for i in range(1, S+1):
 
 header = ['comm','x','y'] + header
 
-filename = spat.comm_filename(S,N,ncomm,bisec,transect,abu,shrt_name)
+filename = comm_filename(S,N,ncomm,bisec,transect,abu,shrt_name)
 
 writer = open(filename,'wb') 
 datawriter = csv.writer(writer)
