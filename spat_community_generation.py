8c76656e47ed48e41d101771ed81df9018f20a46
Ethan White <ethan@weecology.org>
2012-02-02 22:13:25 +0000

    Refactor comm_filename for simplicity and readability.

--- spat_community_generation.py
+++ spat_community_generation.py
@@ -21,12 +21,11 @@ import os
 import mete
 
 def str2bool(v):
-  return v.lower() in ("yes", "true", "t", "1")
+    return v.lower() in ("yes", "true", "t", "1")
 
-def comm_filename(S, N, ncomm, bisec, transect = False, abu = None,
+def comm_filename(S, N, ncomm, bisec, runtype='grid', abu = None,
                    comm_name = None):
-    """Creates the file name of a simulated community produced by the function
-    sim_spatial_whole() in the mete.py module
+    """Get filename for simulated community produced by mete.sim_spatial_whole()
     
     Keyword arguments:
     S -- the number of species
@@ -35,47 +34,18 @@ def comm_filename(S, N, ncomm, bisec, transect = False, abu = None,
     bisec -- the number of bisections
     transect -- a boolean if False then it is assumed a 2D grid was generated
     abu -- the path to an abundance file else this should be None
-    shrt_name -- the short hand name for the output community, this overrides
-                 S and N in the output file name.
+    comm_name -- specific name for the output community
     """
                  
-    if(transect):
-        if(comm_name is None):  
-            if(abu is None):
-                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
-                            str(N) + '_C' + str(ncomm) + '_B' + str(bisec) + 
-                            '_transect.txt')
-            else:
-                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
-                            str(N) + '_empirSAD' + '_C' +  str(ncomm) + '_B' +
-                            str(bisec) + '_transect.txt')
-        else:
-            if(abu is None):
-                filename = ('./comms/simulated_comms_' + comm_name + '_C' +
-                            str(ncomm) + '_B' + str(bisec) + '_transect.txt')
-            else:
-                filename = ('./comms/simulated_comms_' + comm_name + '_empirSAD'
-                            + '_C' + str(ncomm) + '_B' + str(bisec) + 
-                            '_transect.txt')
+    if not comm_name:
+        comm_name = 'S%s_N%s' % (S, N)
+    if abu:
+        empir = '_empirSAD'
     else:
-        if(comm_name is None):
-            if(abu is None):
-                filename = ('./comms/simulated_comms_S' + str(S) + '_N' +
-                            str(N) + '_C' + str(ncomm) + '_B' + str(bisec) +
-                            '_grid.txt')
-            else:
-                filename = ('./comms/simulated_comms_S' + str(S) + '_N' + 
-                            str(N) + '_empirSAD' + '_C' + str(ncomm) + '_B' +
-                            str(bisec) + '_grid.txt')
-        else:
-            if(abu is None):
-                filename = ('./comms/simulated_comms_' + comm_name + '_C' +
-                            str(ncomm) + '_B' + str(bisec) + '_grid.txt')
-            else:
-                filename = ('./comms/simulated_comms_' + comm_name + '_empirSAD'
-                            + '_C' + str(ncomm) + '_B' + str(bisec) + '_grid.txt')
-    return(filename)
-
+        empir = ''
+    return './comms/simulated_comms_%s%s_C%s_B%s_%s.txt' % (comm_name,
+                                                                empir, ncomm,
+                                                                bisec, runtype)
 
 if 'comms' not in os.listdir(os.path.curdir):
     os.mkdir('comms')
