2a325314843fb763c64f8f7cca87df9e38ee4afe
Dan McGlinn <danmcglinn@gmail.com>
2012-02-11 03:13:58 +0000

    minor updates

--- spat_empir_sar.R
+++ spat_empir_sar.R
@@ -3,6 +3,23 @@
 setwd('/home/danmcglinn/maxent/spat')
 source('spat_sim_vario_func.R')
 
+fileNames = dir('./sar')
+meteFiles = grep('mete',fileNames)
+mete = vector('list',length(meteFiles))
+for(i in seq_along(meteFiles))
+  mete[[i]] = read.csv(paste('./sar/',fileNames[meteFiles[i]],sep=''))
+names(mete) = sub('_mete_sar.txt','',fileNames[meteFiles])
+
+##for cocoli1, cocoli2, sherman1, sherman2 we need to adjust the sars
+##b/c the mete prediction fails at scales where there is less than 1 individual
+toFix = c('cocoli1', 'cocoli2', 'sherman1', 'sherman2')
+toFix = which(names(mete)%in%toFix)
+for(i in seq_along(toFix)){
+  mete[[toFix[i]]]$area = mete[[toFix[i]]]$area * 2
+  mete[[toFix[i]]] = rbind(c(1,NA),mete[[toFix[i]]])
+}
+
+
 fileNames = dir('./data')
 commFiles = grep('comms',fileNames)
 commFiles = commFiles[-c(4,9)] ## needed for the time being until we clear out the old comm files
@@ -27,10 +44,20 @@ for(i in seq_along(dat))
   psp[[i]] = mat2psp(dat[[i]][,-(1:3)],Ns[i],Ms[i])
 names(psp) = names(dat)
 
-## compute SARs
+AminExact = c(1e3/128 * 5e2/64,
+              2e2/128 * 1e2/64,
+              2e2/128 * 1e2/64,
+              1,
+              2e2/128 * 1e2/64,
+              2e2/128 * 1e2/64,
+              (1.4e2/64)^2)
+## compute SARs, non-movinging window 
 sar = vector('list',length(psp))
-for(i in seq_along(psp))
+for(i in seq_along(psp)){
   sar[[i]] = getSAR(psp[[i]],mete[[i]]$area)
+  ## scale area appropriately
+  sar[[i]][,1] = sar[[i]][,1]*AminExact[i]
+}
 names(sar) = names(psp)
 
 ## export results
@@ -38,4 +65,6 @@ for(i in seq_along(sar)){
   write.csv(sar[[i]],file=paste('./sar/',names(sar)[i],'_empir_sar.csv',sep=''),
             row.names=FALSE)
 }
+
+
 
