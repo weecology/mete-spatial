503c68dfaf119296659dd3a918af383160c31f7a
Dan McGlinn <danmcglinn@gmail.com>
2012-03-27 20:39:28 +0000

    Added heap calculations for commonality and improved readability of older code.

--- heap.c
+++ heap.c
@@ -1,23 +1,41 @@
 /*
-Purpose: To compute the HEAP model, Eq. 4.15 in Harte's book
+Purpose: To compute probabilities related to Harte's HEAP model.
+The first set of functions (PiHEAP & PiRecur) carry out computation
+of the probability of observing n in
+
 Arguments:
 n -- the number of individuals
 A -- the area
 no -- the total number of individuals in the pool
 Ao -- the total area of the pool
-prob -- the probability of observing n individuals (what we want back)
+prob -- a probability value
+i -- the number of bisections = log2(Ao/A)
+
+References:
+Harte, J. 2007. Towards a mechanistic basis of unified theory of 
+spatial structure in ecological communities at multiple scales. in 
+Scaling Biodiversity. Eds. Storch et al. 
+
+Harte, J. 2011. Maximum Entropy and Ecology.
 
 */
 
 #include <R.h>
 #include <math.h> 
 
-void HEAP(int *n, double *A, int *no, double *Ao, double *prob){
-    double rHEAP(int n, double A, int no, double Ao) ;
-    *prob = rHEAP(*n,*A,*no,*Ao) ;
+void PiHEAP(int *n, double *A, int *no, double *Ao, double *prob){
+    /*
+    Computes the probability of observing n individuals in a cell 
+    of area A
+    */
+    double PiRecur(int n, double A, int no, double Ao) ;
+    *prob = PiRecur(*n,*A,*no,*Ao) ;
 } 
 
-double rHEAP(int n, double A, int no, double Ao){
+double PiRecur(int n, double A, int no, double Ao){
+    /*
+    Eq. 4.15 in Harte (2011)
+    */
     int i, q ;  
     double total = 0;
     i = log2(Ao / A) ; 
@@ -26,9 +44,57 @@ double rHEAP(int n, double A, int no, double Ao){
     }
     else{
         A = A * 2 ;
-        for (q = n ; q < (no + 1) ; q++){
-            total += rHEAP(q,A,no,Ao) / (q + 1) ;
+        for(q = n ; q < (no + 1) ; q++){
+            total += PiRecur(q,A,no,Ao) / (q + 1) ;
         }
         return total ; 
     }
 }
+
+double lambda(int i, int no){ 
+    /* Eq. 6.4 in Harte (2007) */
+    double PiRecur(int n, double A, int no, double Ao) ; 
+    double A, lamb ;
+    if(no == 0){
+        lamb = 1 ;
+    }
+    else{
+        A = 1/pow(2,i) ;
+        lamb = 1 - PiRecur(0,A,no,1) ;
+    }   
+    return(lamb) ; 
+}
+
+void chiHEAP(int *i, int *j, int *no, double *prob){
+    /*
+    Computes the probability of observing a species in two quadrats of
+    bisected i times and of seperation order j.
+    */
+    double chiRecur(int i, int j, int no) ; 
+    *prob = chiRecur(*i,*j,*no) ; 
+}
+
+double chiRecur(int i, int j, int no){
+    /* Eq. 6.9 and 6.10 in Harte (2007) */
+    double total = 0 ; 
+    int m ;
+    if(no == 1){
+        total += 0 ; 
+    }
+    else{
+        if(j == 1){
+            for(m = 1 ; m < no ; m++){
+                total += lambda(i-1,m) * lambda(i-1,no-m) / (no + 1) ;
+            }
+         }  
+         else{
+             i-- ;
+             j-- ; 
+             for(m = 2 ; m < (no + 1); m++){
+                 total += chiRecur(i,j,m) / (no + 1) ; 
+             }  
+         }
+    }  
+    return(total) ;
+}
+
