b1ad83dcda144b18cd40985b0f57399082ceaf3b
Dan McGlinn <danmcglinn@gmail.com>
2012-06-04 18:24:02 +0000

    Cleaned this code up, the mle functions still take too long to be realisticly deployed across real datasets.

--- spat_bisec_model_log_likelihood.R
+++ spat_bisec_model_log_likelihood.R
@@ -2,86 +2,54 @@
 setwd('/home/danmcglinn/maxent/spat')
 source('spat_analytical_prob_funcs.R')
 
-ll.Bisect = function(n,A,no,Ao,psi){
-  sum(log(sapply(n,function(x) piBisect(x,A,no,Ao,psi))))
+
+piHEAP(5,1,5,4)
+(1/(5+1))^2
+
+piHEAP(4,1,5,4)
+(1/(5+1))*(1/(5+1))+(1/(5+1)*(1/(4+1)))
+
+piHEAP(3,1,5,4)
+(1/(5+1))*(1/(5+1))+(1/(5+1)*(1/(4+1))) + (1/(5+1)*(1/(3+1)))
+
+
+
+
+logFactorialTab = read.delim('logfactorial_lookup_table.txt',header=F)
+factHash = hash(keys = logFactorialTab[,1], values = logFactorialTab[,2])
+
+
+
+
+serp = read.csv('./data/serp_comms.csv')
+dat = as.matrix(serp[serp$grain==16,-(1:3)])
+colSums(dat)
+
+no = 2^(1:9) 
+notimes = NA
+for(i in seq_along(no)){
+  notimes[i] = system.time(piBisect2(0,1,no[i],256,.25))[3]
+  print(no[i])
 }
 
-ll.Bisect2 = function(n,A,no,Ao,psi){
-  tab = table(n)
-  n = as.numeric(names(tab))
-  freq = as.numeric(tab)
-  sum(sapply(1:length(n),function(i) freq[i] * log(piBisect(n[i],A,no,Ao,psi))))
-}
+no = no[1:8]
+plot(log2(no),log2(notimes))
+mod = lm(log2(notimes) ~ log2(no))
+abline(mod)
+round(2^(predict(mod,newdata = data.frame(no = colSums(dat))))/(60),3)
+round(2^(predict(mod,newdata = data.frame(no = colSums(dat))))/(60^2),3)
+round(2^(predict(mod,newdata = data.frame(no = colSums(dat))))/(60^2 * 24),3)
 
-neg.ll.Bisect = function(psi){
-  -sum(log(sapply(dat,function(x) piBisect(x,A,no,Ao,psi))))
-}
 
-neg.ll.Bisect2 = function(psi){
-  tab = table(dat)
-  n = as.numeric(names(tab))
-  freq = as.numeric(tab)
-  -sum(sapply(1:length(n),function(i) freq[i] * log(piBisect(n[i],A,no,Ao,psi))))
-}
 
-bci = read.csv('./data/bci_comms.csv')
-unique(bci$grain)
-bci = bci[bci$grain==3906,]
-bci = bci[,-(1:3)]
-bci = bci[,order(apply(bci,2,sum),decreasing=TRUE)]
-hist(bci[,40])
-dat = bci[,40]
-#dat = sample(bci[,10],2^5)
-dat = dat[sample(length(dat),8)]
-hist(dat)
-A  = 1
-Ao = length(dat)
-no = sum(dat)
-ll.Bisect2(dat,1,no,Ao,.5)
-ll.Bisect2(dat,1,no,Ao,.001)
+dat = dat[,10:11]
 
-##nlm
+fp = function(x) {print(x) ; negllBisectMatrix(x)}
+
 start = proc.time()[3]
-est = nlm(neg.ll.Bisect2,.5)
+est = optimize(fp,c(0,1),tol=1e-3)
 end = proc.time()[3]
-nlmtime = end - start
-##
-fp = function(x) {print(x) ; neg.ll.Bisect2(x)}
-start = proc.time()[3]
-est2 = optimize(fp,c(0,1),tol=1e-3)
-end = proc.time()[3]
-optizetime = end - start
-##
-system.time(optimize(neg.ll.Bisect,c(0,1),tol=1e-3))
 
-plot(table(dat),main=paste('psi est',est$est,sep=' '))
-lines(0:6,sapply(0:6,function(n) piBisect(n,1,no,Ao,est$est))*no,type='o',col='red')
-lines(0:6,sapply(0:6,function(n) piBisect(n,1,no,Ao,.5))*no,type='o',col='blue')
-lines(0:6,sapply(0:6,function(n) piBisect(n,1,no,Ao,.75))*no,type='o',col='green')
-
-
-dat = rpois(2^3,5)
-no = sum(dat)
-Ao = length(dat)
-ll.Bisect(dat,1,no,Ao,.5)
-ll.Bisect(dat,1,no,Ao,.001)
-
-psi = seq(.001,.99,length.out=10)
-plot(psi,sapply(psi,function(j) ll.Bisect(dat,1,no,Ao,j)),type='o')
-
-?nlm
-A = 1
-nlm(neg.ll.Bisect,.25)
-
-dat = randSingle(10,.75,2^4)
-no = sum(dat)
-A = 1
-Ao = length(dat)
-ll.Bisect(dat,1,no,Ao,.5)
-ll.Bisect(dat,1,no,Ao,.001)
-ll.Bisect(dat,1,no,Ao,.75)
-
-nlm(neg.ll.Bisect,.25)
-
-
+write.csv(est,file='./mle/serp_comm_est.csv')
+paste('Optimization took',round(end - start),'seconds')
 
