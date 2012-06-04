a7c3b396efc35b8137a7a31c311346285e4ead1c
Dan McGlinn <danmcglinn@gmail.com>
2012-06-04 18:24:58 +0000

    Updated this script to use the new 'make_comm_matrix' function.

--- spat_serp_gen_comm_mat.R
+++ spat_serp_gen_comm_mat.R
@@ -8,11 +8,37 @@ setwd('/home/danmcglinn/maxent/spat')
 source('spat_sim_vario_func.R')
 
 dat = read.csv('./data/serpentine_data.csv',header=T)
-dat = dat[-(nrow(dat):(nrow(dat)-2)),]
-dat[1,] = ifelse(is.na(dat[1,]),0,dat[1,])
-x = rep(1:16,each=16)
-y = rep(1:16,times=16)
-grains = c(1,4,16)
-comms = quadAggregator(mat = dat, coord = cbind(x,y), grains = grains)
 
-write.csv(comms,file='./data/serp_comms.csv',row.names=FALSE)
+
+i_bisections = c(7:1)
+n_quadrats = 2^i_bisections
+domain = c(1, 17, 1, 17) # defined in number of quadrats here
+
+# prepare data for the make_comm_matrix function
+abu = as.vector(as.matrix(dat))
+S = ncol(dat)
+spnum = rep(1:S, each=256)
+x = rep(rep(1:16, each=16), S)
+y = rep(rep(1:16, times=16), S)
+
+input_dat = data.frame(spnum, x, y, abu)[abu > 0,]
+
+comms1 = make_comm_matrix(input_dat$spnum, S, input_dat[,2:3], n_quadrats, domain,
+                         input_dat$abu)
+
+abu = as.vector(as.matrix(dat))
+S = ncol(dat)
+spnum = rep(1:S, each=256)
+y = rep(rep(1:16, each=16), S)
+x = rep(rep(1:16, times=16), S)
+
+input_dat = data.frame(spnum, x, y, abu)[abu > 0,]
+
+comms2 = make_comm_matrix(input_dat$spnum, S, input_dat[,2:3], n_quadrats, domain,
+                         input_dat$abu)
+
+write.csv(comms, file='./data/serp_comms.csv',row.names=FALSE)
+
+
+      
+  
