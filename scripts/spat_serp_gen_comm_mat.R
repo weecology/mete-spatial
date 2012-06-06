b302032f47a85696026d12d56f9cb0c6423dd262
Dan McGlinn <danmcglinn@gmail.com>
2012-06-06 17:54:56 +0000

    Placed scripts into a directory called scripts and placed presentations into a folder called prsentations.

--- scripts/spat_serp_gen_comm_mat.R
+++ scripts/spat_serp_gen_comm_mat.R
@@ -5,12 +5,11 @@
 
 setwd('/home/danmcglinn/maxent/spat')
 
-source('spat_sim_vario_func.R')
+source('./scripts/spat_sim_vario_func.R')
 
 dat = read.csv('./data/serpentine_data.csv',header=T)
 
-
-i_bisections = c(7:1)
+i_bisections = c(8:4)
 n_quadrats = 2^i_bisections
 domain = c(1, 17, 1, 17) # defined in number of quadrats here
 
@@ -23,22 +22,7 @@ y = rep(rep(1:16, times=16), S)
 
 input_dat = data.frame(spnum, x, y, abu)[abu > 0,]
 
-comms1 = make_comm_matrix(input_dat$spnum, S, input_dat[,2:3], n_quadrats, domain,
-                         input_dat$abu)
-
-abu = as.vector(as.matrix(dat))
-S = ncol(dat)
-spnum = rep(1:S, each=256)
-y = rep(rep(1:16, each=16), S)
-x = rep(rep(1:16, times=16), S)
-
-input_dat = data.frame(spnum, x, y, abu)[abu > 0,]
-
-comms2 = make_comm_matrix(input_dat$spnum, S, input_dat[,2:3], n_quadrats, domain,
+comms = make_comm_matrix(input_dat$spnum, S, input_dat[,2:3], n_quadrats, domain,
                          input_dat$abu)
 
 write.csv(comms, file='./data/serp_comms.csv',row.names=FALSE)
-
-
-      
-  
