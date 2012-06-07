69ec7a57df1461c70d16b1606626c8a86d91c42a
Dan McGlinn <danmcglinn@gmail.com>
2012-06-07 22:11:09 +0000

    Added Oosting dataset and dropped species with zero abundances

--- scripts/spat_empir_sad.R
+++ scripts/spat_empir_sad.R
@@ -1,17 +1,21 @@
-## $Id:$
 ## Purpose: To generate the empirical SAD files
+
+setwd('~/maxent/spat')
 
 ## read in community matrix files
 shrtnames = c('bci', 'cocoli1', 'cocoli2', 'cross', 'sherman1', 'sherman2',
-              'sherman3', 'serp')
+              'sherman3', 'serp', 'oosting')
 comms = vector("list", length=length(shrtnames))
 names(comms) = shrtnames
 for (i in seq_along(comms))
   comms[[i]] = read.csv(paste0('./data/', shrtnames[i], '_comms.csv'))
 
 ## calculate empirical SAD
-sadAbs = sapply(comms,  function(x) 
+sadAbs = sapply(comms, function(x) 
                        apply(x[x$grain == unique(x$grain)[1], -(1:3)], 2, sum))
+
+## drop species with zero abundances
+sadAbs = sapply(sadAbs, function(x) x[x > 0])
 
 ## export SAD files
 for (i in seq_along(comms)) {
