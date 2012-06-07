0dc7f220ba4e5da1eaf5c73180fb51d092892d2e
Dan McGlinn <danmcglinn@gmail.com>
2012-06-07 22:08:09 +0000

    Added the Oosting tree dataset to project

---
+++ scripts/spat_oosting_data_filtering.R
@@ -0,0 +1,25 @@
+## Purpose: to clean up the 19980 Oostings tree dataset for analysis of 
+## 1990 data is used because its pre Hurrican Fran
+## spatial biodiversity patterns. 
+## Metadata: http://esapubs.org/archive/ecol/E088/162/metadata.htm
+
+setwd('~/datasets/oosting/')
+
+oost = read.delim('Oosting_Trees_1998.txt', colClasses = 'character')
+
+## replace the '.' symbols with NA's so that the data can be processed
+
+oost = apply(oost, 2, function(x) ifelse(x == '.', NA, x))
+
+dat = data.frame(ID = as.numeric(oost[,1]),
+                 CODE = as.factor(oost[,2]),
+                 NX = as.numeric(oost[,16]),
+                 NY = as.numeric(oost[,17]),
+                 D90 = as.numeric(oost[,5]),
+                 C90 = as.numeric(oost[,6]))
+
+## only keep the individual trees with a condition code of 1: alive and reasonably intact 
+dat = subset(dat, C90 == 1)
+
+write.csv(dat, 'oosting_trees_1990_filtered.csv', row.names=F)
+
---
+++ scripts/spat_oosting_gen_comm_mat.R
@@ -0,0 +1,21 @@
+## Author: Dan McGlinn
+## Purpose: to create site x species matrices for the oosting tree dataset
+## Metadata: http://esapubs.org/archive/ecol/E088/162/metadata.htm
+
+setwd('~/maxent/spat')
+
+source('./scripts/spat_sim_vario_func.R')
+
+dat = read.csv('~/datasets/oosting/oosting_trees_1990_filtered.csv')
+
+S = length(unique(dat$CODE))
+dat$spnum = as.integer(dat$CODE)
+
+i_bisections = c(12, 10, 8, 6, 4)
+n_quadrats = 2^i_bisections
+domain = c(0,160,0,160)
+
+comms = make_comm_matrix(dat$spnum, S, cbind(dat$NX, dat$NY), n_quadrats, domain)
+
+## output results
+write.csv(comms,file='./data/oosting_comms.csv',row.names=F)
