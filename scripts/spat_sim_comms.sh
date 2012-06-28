9758d38e21d1c8d186f4cde74bb3365303ef70c7
Dan McGlinn <danmcglinn@gmail.com>
2012-06-28 23:34:23 +0000

    Fixed errors in how the jobs were submitted

--- scripts/spat_sim_comms.sh
+++ scripts/spat_sim_comms.sh
@@ -14,29 +14,29 @@ do
     then
        if [ $j == meteSAD ]
        then
-         bsub -q week -J $i -o ./log_files/error_sim_${names[$i]}_$j.log\
-           python spat_community_generation.py ${S[$i]} $N[$i]} 200 ${bisect[$i]}\
+         bsub -q week -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
+           python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\
            False None ${names[$i]} 
        fi
        if [ $j == empirSAD ]
        then
-         bsub -q week -J $i -o ./log_files/error_sim_${names[$i]}_$j.log\
-           python spat_community_generation.py ${S[$i]} $N[$i]} 200 ${bisect[$i]}\ 
-           False ../data/${names[$i]}_sad.csv ${names[$i]}
+         bsub -q week -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
+           python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\
+           False "../data/${names[$i]}_sad.csv" ${names[$i]}
        fi
     fi
     if [ $1 == usu ]
     then
       if [ $j == meteSAD ] 
       then
-        python spat_community_generation.py ${S[$i]} ${N[$i]} 10 ${bisect[$i]}\
+        python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\
         False None ${names[$i]}\
         > log_files/error_sim_${names[$i]}_$j.log 2>&1 &
       fi
       if [ $j == empirSAD ]
       then
         python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\ 
-        False ../data/${names[$i]}_sad.csv ${names[$i]}\
+        False "../data/${names[$i]}_sad.csv" ${names[$i]}\
         > log_files/error_sim_${names[$i]}_$j.log 2>&1 &            
       fi
     fi
