25f5d74efdc01ddbd5aebe2fffaf39abd873b6f7
Dan McGlinn <danmcglinn@gmail.com>
2012-06-28 21:05:05 +0000

    Added bash script that generates the simulated communities which are parameterized from the empirical data

---
+++ scripts/spat_sim_comms.sh
@@ -0,0 +1,40 @@
+indices=$2
+names=( `cat ../data/shrtnames.txt`)
+bisect=( `cat ../data/bisect_fine.txt`)
+S=( `cat ../data/S_vals.txt`) 
+N=( `cat ../data/N_vals.txt`) 
+
+sadType="meteSAD empirSAD"
+
+for i in $indices
+do
+  for j in $sadType
+  do
+    if [ $1 == unc ]
+    then
+       if [ $j == meteSAD ]
+       then
+         bsub -q week -J $i -o. /log_files/error_sim_${names[$i]}_$j.log\
+           python spat_community_generation.py ${S[$i]} $N[$i]} 200 ${bisect[$i]} False None ${names[$i]} 
+       fi
+       if [ $j == empirSAD ]
+       then
+         bsub -q week -J $i -o. /log_files/error_sim_${names[$i]}_$j.log\
+           python spat_community_generation.py ${S[$i]} $N[$i]} 200 ${bisect[$i]} False\
+           ../data/${names[$i]}_sad.csv $(names[$i]}
+       fi
+    fi
+    if [ $1 == usu ]
+    then
+      if [ $j == meteSAD ] 
+      then
+        python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]} False None ${names[$i]} &
+      fi
+      if [ $j == empirSAD ]
+      then
+        python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]} False\ 
+          ../data${names[$i]}_sad.csv ${names[$i]} &            
+      fi
+    fi
+  done
+done
