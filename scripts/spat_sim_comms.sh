8a43a7f294825e81d1686825eddadf261ba3bd56
Dan McGlinn <danmcglinn@gmail.com>
2012-07-02 17:29:13 +0000

    Added a memory limit specification for jobs submitted to the unc cluster

--- scripts/spat_sim_comms.sh
+++ scripts/spat_sim_comms.sh
@@ -14,13 +14,13 @@ do
     then
        if [ $j == meteSAD ]
        then
-         bsub -q week -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
+         bsub -q week -M 8 -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
            python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\
            False None ${names[$i]} 
        fi
        if [ $j == empirSAD ]
        then
-         bsub -q week -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
+         bsub -q day -M 8 -J ${names[$i]} -o ./log_files/error_sim_${names[$i]}_$j.log\
            python spat_community_generation.py ${S[$i]} ${N[$i]} 200 ${bisect[$i]}\
            False "../data/${names[$i]}_sad.csv" ${names[$i]}
        fi
