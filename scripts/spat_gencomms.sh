5baef074e1ba587ba1fcab958cad8ba2ea302372
Dan McGlinn <danmcglinn@gmail.com>
2012-07-02 19:47:19 +0000

    Updated so that it interacts with spat_gencomms.py

--- scripts/spat_gencomms.sh
+++ scripts/spat_gencomms.sh
@@ -1,21 +1,14 @@
-for S in 10  11  13  14  16  18  21  23  26  30  34  38  43  48  55  62  70  78  89 100
+indices=$2
 
-
-
+for i in $indices
 do
- 
- 
-  for N in 120 186 289 447 694 1076 1668 2587 4011 6220 9646 14957 23193 35965 55769 86479 134099 207941 322444 500000
-
-
-  do
-
-
-    python spat_community_generation.py $S $N 200 13 False None space &
-
-    sleep 20
-
-  done
+  if [ $1 == unc ]
+  then
+    bsub -q day -M 6 -J parm_space -o ./log_files/error_sim_S$i.log\
+    python spat_gencomms.py $i
+  fi
+  if [ $1 == usu ] 
+  then
+    python spat_gencomms.py $i > ./log_files/error_sim_S$i.log
+  fi
 done
-
-
