161cd07fca220494e675b1da674dbf57254a28b3
Dan McGlinn <danmcglinn@gmail.com>
2012-07-02 20:42:57 +0000

    Change to ensure that the Svals were handled as a list

--- scripts/spat_gencomms.py
+++ scripts/spat_gencomms.py
@@ -9,6 +9,6 @@ Nvals = [120, 186, 289, 447, 694, 1076, 1668, 2587, 4011, 6220, 9646, 14957,
 
 if len(sys.argv) > 1:
     Sindex  = int(sys.argv[1]) 
-    Svals = Svals[Sindex]
+    Svals = [Svals[Sindex]]
 
 sg.explore_parameter_space(Svals, Nvals, 200, 12)
