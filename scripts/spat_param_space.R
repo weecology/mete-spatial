c12d7ad3240216d7480cd93e4bd36b166be81226
Dan McGlinn <danmcglinn@gmail.com>
2012-07-05 21:24:19 +0000

    Major update that uses the new functions in spat_sim_vario_func.R to load and average the simulated results. Additionally fixed error in how the richness and abundance values were compared agains the array of results so that the correct comparison is now made, improved plotting and saving of results

--- scripts/spat_param_space.R
+++ scripts/spat_param_space.R
@@ -1,144 +1,215 @@
-setwd('c:/users/dan mcglinn/documents/lab data/maxent/spat/')
-source('spat_sim_vario_func.R')
+options(scipen=10) 
 
-files = dir('./sorensen/')
+setwd('~/maxent/spat/')
+source('./scripts/spat_sim_vario_func.R')
 
-S <- round(10^seq(log10(10),log10(100),length.out=20))
-N <- round(10^seq(log10(120),log10(5e5),length.out=20))
+S = round(10^seq(log10(10), log10(100), length.out=20))
+N = round(10^seq(log10(120), log10(5e5), length.out=20))
 
-results = vector('list',length(S)*length(N))
-icount = 0
-for(s in S){
-  for(n in N){
-    icount = icount + 1
-    fileSuffix = paste('_S',s,'_N',n,sep='')
-    fileName = files[grep(fileSuffix,files)]
-    if(length(fileName) == 0){
-      next 
-    }
-    load(file.path('./sorensen/',fileName))
-    results[[icount]] = sorensen
-    rm(sorensen)
+simSorAbu = loadSimResults(S, N, './sorensen/')
+simSorAbuAvg = avgSimResults(simSorAbu, 'sorensen')
+
+#save(simSorAbuAvg, file='./sorensen/simSorAbuAvg.Rdata')
+#load('./sorensen/simSorAbuAvg.Rdata')
+
+stats = getSimStats(simSorAbuAvg, S, N)
+
+pdf('./figs/parm_space_r2_sorensen_abu.pdf', width = 7 * 2, height = 7)
+  lims = range(as.vector(stats[ ,'r2', , , , ]),na.rm=TRUE)
+  nbrks = 12
+  brks = seq(lims[1], lims[2], length.out=nbrks)
+  par(mfrow=c(1, 2))
+  for (meth in c('ols', 'wtr')) { 
+    hist(stats['exp', 'r2', meth, , , ], breaks = brks, ylim=c(0, 1.7e3), main=meth,
+         col='red', xlab=expression(R^2 * ' of model'))
+    hist(stats['pwr', 'r2', meth, , , ], breaks = brks, ylim=c(0, 1.7e3), 
+         add=TRUE, col='blue')
+    legend('topleft', c('Exponential Model', 'Power Model'), col=c('red', 'blue'),
+           lwd=8, cex=2, bty='n')
   }
-}
+dev.off()
 
-simSorAbu = reshapeResults(results,'sorensen')
 
-combine = vector('list',length=length(simSorAbu))
-for(i in seq_along(combine)){
-  if(!is.null(simSorAbu[[i]]))
-    combine[[i]] = rep(1,nrow(simSorAbu[[i]]))
-}
+pwrStats = drop(stats['pwr', , , , ,])
 
-simSorAbuAvg = avgResults(simSorAbu,combine)
+## plot intercept,slope,and R2 for pwr model of DD at each grain
 
-results = simSorAbuAvg
-stats = array(NA,dim=c(2,3,length(S),length(N)))
-dimnames(stats)[[1]] = c('exp','pwr')
-dimnames(stats)[[2]] = c('b0','b1','r2')
-dimnames(stats)[[3]] = S
-dimnames(stats)[[4]] = N
-i = 0
-for(s in seq_along(S)){
-  for(n in seq_along(N)){
-    i = i + 1
-    if(is.null(results[[i]]))
-      next
-    logmod = lm(Metric ~ log10(Dist),data=results[[i]])
-    pwrmod = lm(log10(Metric) ~ log10(Dist),data = results[[i]])
-    stats[1,1:2,s,n] = coef(logmod)
-    stats[1,3,s,n] = summary(logmod)$r.squ
-    stats[2,1:2,s,n] = coef(pwrmod)
-    stats[2,3,s,n] = summary(pwrmod)$r.squ
+pdf('./figs/stats_sor_abu_all_grains_pwr_wtr_.pdf', width=7 * 2, height=7)
+  for (g in seq_along(grains)) {
+    par(mfrow=c(1, 3))
+    for (i in 1:3) {
+      image(pwrStats[i, 'wtr', g, , ])
+    }  
   }
-}
+dev.off()
 
-lims = range(as.vector(stats[,3,,]),na.rm=TRUE)
-nbrks = 12
-brks = seq(lims[1],lims[2],length.out=nbrks)
-par(mfrow=c(1,1))
-hist(stats[1,3,,],breaks = brks,ylim=c(0,200),main='',col='red',
-     xlab=expression(R^2*' of model'))
-hist(stats[2,3,,],breaks = brks,ylim=c(0,200),main='Power Model',add=TRUE,col='blue')
-legend('topleft',c('Exponential Model','Power Model'),col=c('red','blue'),lwd=8,
-       cex=2,bty='n')
-
-pwrStats = drop(stats[2,,,])
-
-## plot intercept,slope,and R2 for pwr model of DD
-par(mfrow=c(1,3))
-for(i in 1:3){
-  image(pwrStats[i,,1:11])
-}
+pdf('./figs/stats_sor_abu_grain1_pwr_wtr_.pdf', width=7 * 2, height=7)
+  par(mfrow=c(1, 3))
+  for (i in 1:3) {
+    image(pwrStats[i, 'wtr', 1, , ])
+  }  
+dev.off()
 
 ##create legend figure
 par(mfrow=c(1,3))
 for(i in 1:3){
-  tmp = as.vector(pwrStats[i,,1:11])
-  x = round(seq(min(tmp,na.rm=T),max(tmp,na.rm=T),length.out=7),3)
+  tmp = as.vector(pwrStats[i, 'ols', 1, , ])
+  x = round(seq(min(tmp,na.rm=T), max(tmp,na.rm=T), length.out=7), 3)
   image(matrix(x),axes=F)
   axis(side=1,at=seq(0,1,length.out=7),labels=x,tick=F,cex.axis=2)
 }
 
-svals = rep(S,each = length(N))
-nvals = rep(N,length(S))
-ratio = log10(nvals/svals)
-ratio = array(ratio,dim=c(length(S),length(N)))
+svals = rep(S, length(N))
+nvals = rep(N, each=length(S))
 
-#pdf('dd_coef_n_over_s.pdf')
-lwd = 3
-par(mfrow=c(2,3))
-xlab = 'log10(N/S)'
-plot(ratio,pwrStats[1,,],xlab=xlab,ylab='Intercept',pch=19)
-plot(ratio,pwrStats[1,,],xlab=xlab,ylab='Intercept',type='n')
-for(s in seq_along(S))
-  lines(ratio[s,],pwrStats[1,s,],col='palevioletred',lwd=lwd)
-plot(ratio,pwrStats[1,,],xlab=xlab,ylab='Intercept',type='n')
-for(n in seq_along(N))
-  lines(ratio[,n],pwrStats[1,,n],col='dodgerblue',lwd=lwd)
-##
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',pch=19)
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',type='n')
-for(s in seq_along(S))
-  lines(ratio[s,],pwrStats[2,s,],col='palevioletred',lwd=lwd)
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',type='n')
-for(n in seq_along(N))
-  lines(ratio[,n],pwrStats[2,,n],col='dodgerblue',lwd=lwd)
-#dev.off()
-#### same as above but for presentation
-lwd = 3
-par(mfrow=c(2,3))
-plot(ratio,pwrStats[1,,],xlab='',ylab='',pch=19)
-plot(ratio,pwrStats[1,,],xlab='',ylab='',type='n')
-for(s in seq_along(S))
-  lines(ratio[s,],pwrStats[1,s,],col='palevioletred',lwd=lwd)
-plot(ratio,pwrStats[1,,],xlab='',ylab='',type='n')
-for(n in seq_along(N))
-  lines(ratio[,n],pwrStats[1,,n],col='dodgerblue',lwd=lwd)
-##
-plot(ratio,pwrStats[2,,],xlab='',ylab='',pch=19)
-plot(ratio,pwrStats[2,,],xlab='',ylab='',type='n')
-for(s in seq_along(S))
-  lines(ratio[s,],pwrStats[2,s,],col='palevioletred',lwd=lwd)
-plot(ratio,pwrStats[2,,],xlab='',ylab='',type='n')
-for(n in seq_along(N))
-  lines(ratio[,n],pwrStats[2,,n],col='dodgerblue',lwd=lwd)
+pdf('./figs/log_n_over_s_coef_sor_abu_grain1_pwr_wtr.pdf', width = 7 * 2, height = 7)
+  ratio = log(nvals / svals)
+  ratio = array(ratio, dim=c(length(S), length(N)))
+  xlab = 'log(N/S)'
+  g = 1
+  meth = 'wtr'
+  lwd = 3
+  par(mfrow=c(2,3))
+  plot(ratio, pwrStats['b0', meth, g, , ], xlab=xlab, ylab='Intercept', pch=19)
+  plot(ratio, pwrStats['b0', meth, g, , ], xlab=xlab, ylab='Intercept', type='n')
+  for (s in seq_along(S))
+    lines(ratio[s, ], pwrStats['b0', meth, g, s, ], col='palevioletred', lwd=lwd)
+  plot(ratio, pwrStats['b0', meth, g, , ], xlab=xlab, ylab='Intercept', type='n')
+  for (n in seq_along(N))
+    lines(ratio[ , n], pwrStats['b0', meth, g, , n], col='dodgerblue', lwd=lwd)
+  ##
+  plot(ratio, pwrStats['b1', meth, g, , ], xlab=xlab, ylab='Slope', pch=19)
+  plot(ratio, pwrStats['b1', meth, g, , ], xlab=xlab, ylab='Slope', type='n')
+  for (s in seq_along(S))
+    lines(ratio[s, ], pwrStats['b1', meth, g, s, ], col='palevioletred', lwd=lwd)
+  plot(ratio, pwrStats['b1', meth, g, , ], xlab=xlab, ylab='Slope', type='n')
+  for (n in seq_along(N))
+    lines(ratio[ , n], pwrStats['b1', meth, g, , n], col='dodgerblue', lwd=lwd)
+dev.off()
+
+## consider 3 of the grains
+pdf('./figs/log_n_over_s_coef_sor_abu_3grains_pwr_wtr.pdf', width = 7 * 2,
+    height = 7 * 2)
+  ratio = log(nvals / svals)
+  ratio = array(ratio, dim=c(length(S), length(N)))
+  xlab = 'log(N/S)'
+  par(mfrow=c(3, 3))
+  for (cof in c('b0', 'b1')) {
+    for (g in c(1, 3, 5)) {
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, pch=19,
+           main = paste('Grain is', grains[g], 'of', 2^12, sep=' '))
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(s in seq_along(S))
+        lines(ratio[s, ], pwrStats[cof, meth, g, s, ], col='palevioletred', 
+              lwd=lwd)
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(n in seq_along(N))
+        lines(ratio[ , n],pwrStats[cof, meth, g, , n], col='dodgerblue', lwd=lwd)
+    }  
+  }  
+dev.off()
+
+## overlap the grains onto a single panel
+pdf('./figs/log_n_over_s_coef_sor_abu_all_grains_overlap_pwr_wtr.pdf', 
+    width = 7 * 2, height = 7)
+  ratio = log(nvals / svals)
+  ratio = array(ratio, dim=c(length(S), length(N)))
+  xlab = 'log(N/S)'
+  par(mfrow=c(1, 2))
+  for (cof in c('b0', 'b1')) {
+    plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n',
+         ylim = range(pwrStats[cof, meth, , , ], na.rm=T))
+      for (g in seq_along(grains)) {
+        points(ratio, pwrStats[cof, meth, g, , ], col=g, pch=1)
+      }  
+    if (cof == 'b0')
+      legend('bottomright', legend=c('Grains', grains), col=c(NA, 1:length(grains)),
+             pch=1, bty='n', cex = 2, lwd=3, lty=NA)
+  }  
+dev.off()
 
 
-###Scale collapse attempt
-ratio = log10(nvals/svals) - (svals *log10(svals))
-ratio = array(ratio,dim=c(length(S),length(N)))
-xlab = 'log10(N/S) - (S * log10(S))'
-par(mfrow=c(1,3))
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',pch=19,xlim=c(-60,0))
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',type='n',xlim=c(-60,0))
-for(s in seq_along(S))
-  lines(ratio[s,],pwrStats[2,s,],col='palevioletred',lwd=lwd)
-plot(ratio,pwrStats[2,,],xlab=xlab,ylab='Slope',type='n',xlim=c(-60,0))
-for(n in seq_along(N))
-  lines(ratio[,n],pwrStats[2,,n],col='dodgerblue',lwd=lwd)
 
+## consider log log
+pdf('./figs/log_log_n_over_s_coef_sor_abu_3grains_pwr_wtr.pdf', width = 7 * 2,
+    height = 7 * 2)
+  ratio = log(log(nvals / svals))
+  ratio = array(ratio, dim=c(length(S), length(N)))
+  xlab = 'log(log(N/S))'
+  par(mfrow=c(3, 3))
+  for (cof in c('b0', 'b1')) {
+    for (g in c(1, 3, 5)) {
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, pch=19,
+           main = paste('Grain is', grains[g], 'of', 2^12, sep=' '))
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(s in seq_along(S))
+        lines(ratio[s, ], pwrStats[cof, meth, g, s, ], col='palevioletred', 
+              lwd=lwd)
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(n in seq_along(N))
+        lines(ratio[ , n],pwrStats[cof, meth, g, , n], col='dodgerblue', lwd=lwd)
+    }  
+  }  
+dev.off()
 
+## log transforms of N
 
+ratio = log(nvals)
+ratio = array(ratio, dim=c(length(S), length(N)))
+xlab = 'log(N)'
+pdf('./figs/logN_coef_sor_abu_3grains_pwr_wtr.pdf', width = 7 * 2, height = 7 * 2)
+  for (cof in c('b0', 'b1')) {
+    par(mfrow=c(3, 3))
+    for (g in c(1, 3, 5)) {
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, pch=19,
+           main = paste('Grain is', grains[g], 'of', 2^12, sep=' '))
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(s in seq_along(S))
+        lines(ratio[s, ], pwrStats[cof, meth, g, s, ], col='palevioletred', 
+              lwd=lwd)
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(n in seq_along(N))
+        lines(ratio[ , n],pwrStats[cof, meth, g, , n], col='dodgerblue', lwd=lwd)
+    }  
+  }  
+dev.off()
+
+## log transforms of S
+
+ratio = log(svals)
+ratio = array(ratio, dim=c(length(S), length(N)))
+xlab = 'log(S)'
+pdf('./figs/logS_coef_sor_abu_3grains_pwr_wtr.pdf', width = 7 * 2, height = 7 * 2)
+  for (cof in c('b0', 'b1')) {
+    par(mfrow=c(3, 3))
+    for (g in c(1, 3, 5)) {
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, pch=19,
+           main = paste('Grain is', grains[g], 'of', 2^12, sep=' '))
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(s in seq_along(S))
+        lines(ratio[s, ], pwrStats[cof, meth, g, s, ], col='palevioletred', 
+              lwd=lwd)
+      plot(ratio, pwrStats[cof, meth, g, , ], xlab=xlab, ylab=cof, type='n')
+      for(n in seq_along(N))
+        lines(ratio[ , n],pwrStats[cof, meth, g, , n], col='dodgerblue', lwd=lwd)
+    }  
+  }  
+dev.off()
+
+####
+## create flat file to do visual checks that other plots
+## are correct
+meth = 'wtr'
+g = 1
+flat = data.frame(S = svals, N = nvals, ratio = as.vector(ratio), 
+                  b0 = as.vector(pwrStats['b0', meth, g, , ]),
+                  b1 = as.vector(pwrStats['b1', meth, g, , ]))
+
+par(mfrow=c(1,2))
+plot(b0 ~ ratio, data=flat, type='n')
+lines(b0 ~ ratio, data=flat, subset= S == 10)
+plot(b0 ~ ratio, data=flat, type='n')
+lines(b0 ~ ratio, data=flat, subset= N == 120)
+## everytning matches with what the plotting routines below produce
 
 
