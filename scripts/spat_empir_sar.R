37a55652a0ecbacc513484ca7705d941f9fbd42a
Dan McGlinn <danmcglinn@gmail.com>
2012-07-12 16:36:23 +0000

    Dropped the loading of the METE sar information, scales of consideration are now parameterized by the files used in the shell scripts, a flat empirical sar file is also now produced with all the datasets in it.

--- scripts/spat_empir_sar.R
+++ scripts/spat_empir_sar.R
@@ -1,68 +1,67 @@
 ## Purpose: to compute the empirical SARs
 
-setwd('/home/danmcglinn/maxent/spat')
-source('spat_sim_vario_func.R')
-
-fileNames = dir('./sar')
-meteFiles = grep('mete',fileNames)
-mete = vector('list',length(meteFiles))
-for(i in seq_along(meteFiles))
-  mete[[i]] = read.csv(paste('./sar/',fileNames[meteFiles[i]],sep=''))
-names(mete) = sub('_mete_sar.txt','',fileNames[meteFiles])
-
-##for cocoli1, cocoli2, cross, sherman1, sherman2 we need to adjust the sars
-##b/c the mete prediction fails at scales where there is less than 1 individual
-toFix = c('cocoli1', 'cocoli2', 'cross', 'sherman1', 'sherman2')
-toFix = which(names(mete)%in%toFix)
-for(i in toFix){
-  mete[[i]]$area = mete[[i]]$area * 2
-  mete[[i]] = rbind(c(1,NA),mete[[i]])
-}
+setwd('~/maxent/spat')
+source('./scripts/spat_sim_vario_func.R')
 
 fileNames = dir('./data')
-commFiles = grep('comms',fileNames)
-## First aggregate community files into a single list
-dat = vector('list',length(commFiles))
-for(i in seq_along(commFiles))
-  dat[[i]] = as.matrix(read.csv(paste('./data/',fileNames[commFiles[i]],sep='')))
-names(dat) = sub('_comms.csv','',fileNames[commFiles])
+commFiles = grep('comms', fileNames)
 
-Amin = unlist(lapply(dat,function(x)unique(x[,1])[1]))
-for(i in seq_along(dat))
-  dat[[i]] = (dat[[i]][dat[[i]][,1] == Amin[i],])
-gc()
+## First aggregate community files into a single list
+dat = vector('list', length(commFiles))
+names(dat) = sub('_comms.csv', '', fileNames[commFiles])
+for (i in seq_along(commFiles))
+  dat[[i]] = as.matrix(read.csv(paste('./data/', fileNames[commFiles[i]], sep='')))
+
+Amin = unlist(lapply(dat, function(x) unique(x[ , 1])[1]))
+for (i in seq_along(dat))
+  dat[[i]] = (dat[[i]][dat[[i]][ , 1] == Amin[i], ])
+
+## load empirical constants
+shrtnames = read.table('./data/shrtnames.txt', colClasses='character')
+grain_fine = read.table('./data/grain_fine.txt')
+bisect_fine = read.table('./data/bisect_fine.txt')
+bisect_coarse = read.table('./data/bisect_coarse.txt')
+
+proper_order = match(names(dat), shrtnames)
+grain_fine = grain_fine[proper_order]
+bisect_fine = bisect_fine[proper_order]
+bisect_coarse = bisect_coarse[proper_order]
 
 ## convert them to multidimensional arrays
-unlist(lapply(dat,nrow))
-Ns = c(rep(128,3),64,16,rep(128,2),64)
-Ms = c(rep(64,4),16,rep(64,2),64)
-psp = vector('list',length(dat))
-for(i in seq_along(dat))
-  psp[[i]] = mat2psp(dat[[i]][,-(1:3)],Ns[i],Ms[i])
-names(psp) = names(dat)
 
-AminExact = c(1e3/128 * 5e2/64,
-              2e2/128 * 1e2/64,
-              2e2/128 * 1e2/64,
-              2e2/64 * 2e2/64,
-              1,
-              2e2/128 * 1e2/64,
-              2e2/128 * 1e2/64,
-              (1.4e2/64)^2)
+Ns = n_pixels_long(bisect_fine)
+Ms = n_pixels_wide(bisect_fine)
+psp = vector('list',length(dat))
+names(psp) = names(dat)
+for(i in seq_along(dat))
+  psp[[i]] = mat2psp(dat[[i]][ ,-(1:3)], Ns[i], Ms[i])
+
 ## compute SARs, non-movinging window 
+grains = sapply(1:length(psp), function(x) 2^(0:bisect_fine[1, x]))
+
 sar = vector('list',length(psp))
-for(i in seq_along(psp)){
-  sar[[i]] = getSAR(psp[[i]],mete[[i]]$area)
-  ## scale area appropriately
-  sar[[i]][,1] = sar[[i]][,1]*AminExact[i]
-}
 names(sar) = names(psp)
+for (i in seq_along(psp)) {
+  sar[[i]] = getSAR(psp[[i]], grains[[i]])
+  ## add area m2 column
+  sar[[i]] = data.frame(sar[[i]], area = sar[[i]][ , 1] * grain_fine[1, i])
+}
 
 ## export results
-for(i in seq_along(sar)){
-  write.csv(sar[[i]],file=paste('./sar/',names(sar)[i],'_empir_sar.csv',sep=''),
-            row.names=FALSE)
+for (i in seq_along(sar)) 
+  write.csv(sar[[i]], file=paste('./sar/', names(sar)[i], '_empir_sar.csv', 
+                                sep=''), row.names=FALSE)
+
+## create flat file of sar data
+for (i in seq_along(sar)) {
+  if (i == 1)
+    dat = data.frame(site=names(sar)[i], sar[[i]])
+  else
+    dat = rbind(dat, data.frame(site=names(sar)[i], sar[[i]]))
 }
+
+write.csv(dat, file='./sar/empir_sars.csv', row.names=FALSE)
+
 
 
 
