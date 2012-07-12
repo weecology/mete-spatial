2d93f6bd8623064704fa71cebed2386102a6c1bb
Dan McGlinn <danmcglinn@gmail.com>
2012-07-12 17:54:57 +0000

    Modified script so that the only argument that must be supplied ad the command line is the site name of interest.  S, N, and the number of bisections are then pulled from the empir_sars.csv file.  A criteria is enforced that grains in which the density of individuals is less than one are not considered in this analysis.

--- scripts/spat_mete_sar.py
+++ scripts/spat_mete_sar.py
@@ -15,19 +15,29 @@ if 'sar' not in os.listdir('..'):
     os.mkdir('../sar')
 
 if(len(sys.argv) > 1):
-    S = int(sys.argv[1]) 
-    N = int(sys.argv[2]) 
-    bisec = int(sys.argv[3])
-    shrt_name = sys.argv[4]
+    shrt_name = sys.argv[1]
 else:
     S = 10
     N = 100
     bisec = 4
     shrt_name = 'test'
     
-Amax = 2 ** bisec  # number of quadrats per community 
-Amin = 1
- 
+datafile = open('../sar/empir_sars.csv', 'r')
+datareader = csv.reader(datafile)
+data = []
+for row in datareader:
+    data.append(row)
+
+for i in range(1, len(data)):
+    data[i][1 : ] = map(float, data[i][1 : ])
+
+indices = mete.which([data[i][0] == shrtname and data[i][3] > 1 for i in range(0, len(data))])
+
+Amin = min([int(data[i][1]) for i in indices])
+Amax = max([int(data[i][1]) for i in indices])
+S = max([int(data[i][2]) for i in indices])
+N = max([int(data[i][3]) for i in indices])
+
 sar_down = mete.downscale_sar(Amax, S, N, Amin)
 
 # Make an array so that the data is easier to output
