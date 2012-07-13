c36ab93cd903c06edaf32a902e1397d9c3423506
Dan McGlinn <danmcglinn@gmail.com>
2012-07-13 00:20:12 +0000

    Updated with additional site names

--- scripts/spat_sar_slopes.py
+++ scripts/spat_sar_slopes.py
@@ -11,35 +11,35 @@ import os
 import mete
 
 
-data_names = ['bci','cocoli1','cocoli2','cross','sherman1','sherman2',
+site_names = ['bci','cocoli1','cocoli2','cross','sherman1','sherman2',
               'sherman3', 'serp', 'oosting', 'ferp', 'luquillo', 'graveyard',
               'landsend', 'rocky', 'bormann', 'woodbridge', 'baldmnt', 'bryan',
               'bigoak']
 
 slopes = []
-for shrt_name in data_names:
-    datafile = open('./sar/' + shrt_name + '_empir_sar.csv', 'r')
+for shrt_name in site_names:
+    datafile = open('../sar/' + shrt_name + '_empir_sar.csv', 'r')
     datareader = csv.reader(datafile)
     data = []
     for row in datareader:
         data.append(row)
    
     site_data = []
-    for i in range(1,len(data)):
+    for i in range(1, len(data)):
         site_data.append(data[i][0:3])
 
     site_data = [map(float,x) for x in site_data]
 
     slopes.append(mete.get_slopes(site_data))
 
-writer = open('./sar/sar_slopes.csv','wb') 
+    print shrt_name
+
+writer = open('../sar/sar_slopes.csv', 'wb') 
 datawriter = csv.writer(writer)
-datawriter.writerow(['comm','area', 'obsZ', 'predZ', 'NS'])
-for i in range(0,len(data_names)):
-    for j in range(0,len(slopes[i])):
-        slopes[i][j].insert(0,data_names[i])
+datawriter.writerow(['comm', 'area', 'obsZ', 'predZ', 'NS'])
+for i in range(0, len(site_names)):
+    for j in range(0, len(slopes[i])):
+        slopes[i][j].insert(0, site_names[i])
         datawriter.writerow(slopes[i][j])
 
-writer.close()  
-
-
+writer.close()
