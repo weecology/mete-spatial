cd6368b26ce111e34b69302ed874310337c52c7e
Dan McGlinn <danmcglinn@gmail.com>
2012-11-10 05:05:15 +0000

    Updated such that the number of processors and permutations are given when abundance DDR patterns are computed

--- scripts/spat_empir_analysis.sh
+++ scripts/spat_empir_analysis.sh
@@ -1,6 +1,8 @@
-commName=$2
+npar=$2
+nperm=$3
+commName=$4
 
-metricsToCalc="sorensen varWithin"
+metricsToCalc="sorensen"
 
 dataType="abu binary"
 
@@ -12,8 +14,17 @@ then
     do 
       for k in $dataType 
       do
-        bsub -q week -x -J $i -o ./log_files/error_$j_$k_$i.log\
-        Rscript spat_empir_analysis.R $i $j $k 
+        if [ $k == abu ]
+        then
+          export OMP_NUM_THREADS=$npar
+          bsub -q week -M 20 -n $npar -R "span[hosts=1]" -J $i\
+          -o ./log_files/error_$j_$k_$i.log\
+          Rscript spat_empir_analysis.R $i $j $k $nperm $npar
+        else
+          export OMP_NUM_THREADS=1
+          bsub -q week -M 20 -n 1 -J $i -o ./log_files/error_$j_$k_$i.log\
+          Rscript spat_empir_analysis.R $i $j $k NULL 1
+        fi
       done
     done
   done
@@ -27,7 +38,14 @@ then
     do 
       for k in $dataType 
       do
-        Rscript spat_empir_analysis.R $i $j $k >./log_files/error_$j_$k_$i.log 2>&1 &
+        if [ $k == abu ]
+        then
+          Rscript spat_empir_analysis.R $i $j $k $nperm $npar\
+           >./log_files/error_$j_$k_$i.log 2>&1 &
+        else
+          Rscript spat_empir_analysis.R $i $j $k NULL 1\
+          >./log_files/error_$j_$k_$i.log 2>&1 &
+        fi
       done
     done
   done
