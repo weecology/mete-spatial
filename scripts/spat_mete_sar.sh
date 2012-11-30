6d2663a984bea57e6b08983bf43b3751cfecf939
Dan McGlinn <danmcglinn@gmail.com>
2012-11-30 03:57:26 +0000

    Fixed minor syntax mistakes

--- scripts/spat_mete_sar.sh
+++ scripts/spat_mete_sar.sh
@@ -1,15 +1,16 @@
 indices=$2
 names=( `cat ../data/shrtnames.txt`)
-
 for i in $indices
 do
   if [ $1 == unc ]
+  then
     bsub -q week -M 8 -J ${names[$i]}\
-    -o ./log_files/error_mete_sar_${names[$i]}.log\ 
-    python spat_mete_sar.py ${names[$i]}\
+    -o ./log_files/error_mete_sar_${names[$i]}.log\
+    python spat_mete_sar.py ${names[$i]}
   fi
   if [ $1 == usu ]
+  then
     python spat_mete_sar.py ${names[$i]}\
-    > ./log_files/error_mete_sar_${names[$i]}.log 2>&1 &  
+    >./log_files/error_mete_sar_${names[$i]}.log 2>&1 &  
   fi
 done
