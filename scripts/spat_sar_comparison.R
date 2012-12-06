1c99c1ed68d6fd09359aa9c8d88c7d31998e9d71
Dan McGlinn <danmcglinn@gmail.com>
2012-12-06 03:12:02 +0000

    Major revision, this script now only plots the simulated and empirical SARs and compares their residuals

--- scripts/spat_sar_comparison.R
+++ scripts/spat_sar_comparison.R
@@ -3,222 +3,235 @@
 setwd('~/maxent/spat')
 source('./scripts/spat_sim_vario_func.R')
 
-fileNames = dir('./sar')
+## load data
+source('./scripts/spat_sar_load_and_avg_data.R')
 
-empirFiles = grep('empir_sar.csv', fileNames)
-empir = vector('list', length(empirFiles))
-names(empir) = sub('_empir_sar.csv', '', fileNames[empirFiles])
-for (i in seq_along(empirFiles))
-  empir[[i]] = read.csv(paste('./sar/', fileNames[empirFiles[i]], sep=''))
-
-meteFiles = grep('mete_sar.txt', fileNames)
-mete = vector('list', length(meteFiles))
-names(mete) = sub('_mete_sar.txt', '', fileNames[meteFiles])
-for (i in seq_along(meteFiles)) {
-  mete[[i]] = read.csv(paste('./sar/', fileNames[meteFiles[i]], sep=''))
-  mete[[i]]$area = mete[[i]]$area * empir[[i]]$area[1]
-}
-
-meteavgFiles = grep('mete_sar_avgs.csv', fileNames)
-meteavg = vector('list', length(meteavgFiles))
-names(meteavg) = sub('_mete_sar_avgs.csv', '', fileNames[meteavgFiles])
-for (i in seq_along(meteavgFiles)) {
-  meteavg[[i]] = read.csv(paste('./sar/', fileNames[meteavgFiles[i]], sep=''))
-  meteavg[[i]] = meteavg[[i]][ , -1]
-  Amin = empir[[match(names(meteavg)[i], names(empir))]]$area[1]
-  meteavg[[i]]$grains = meteavg[[i]]$grains * Amin
-}
-
-## average cocoli and sherman plots
-empir$sherman1 = (empir$sherman1 + empir$sherman2) / 2
-empir$cocoli1 = (empir$cocoli1 + empir$cocoli2) / 2
-mete$sherman1 = (mete$sherman1 + mete$sherman2) / 2
-mete$cocoli1 = (mete$cocoli1 + mete$cocoli2) / 2
-meteavg$sherman1 = (meteavg$sherman1 + meteavg$sherman2) / 2
-meteavg$cocoli1 = (meteavg$cocoli1 + meteavg$cocoli2) / 2
-empir = empir[-match(c('sherman2','sherman3','cocoli2'), names(empir))]
-mete = mete[-match(c('sherman2','sherman3','cocoli2'), names(mete))]
-meteavg = meteavg[-match(c('sherman2','cocoli2'), names(meteavg))]
-
-## load expected sars under random placement
-load('./sar/expected_empir_sars.Rdata')
-
-addCI = function(x, y.lo, y.hi, col, data=NULL) {
-  if (!is.null(data)) {
-    x = eval(parse(text=paste(data, '$', x, sep='')))
-    y.lo = eval(parse(text=paste(data, '$', y.lo, sep='')))
-    y.hi = eval(parse(text=paste(data, '$', y.hi, sep='')))
-  }  
-  xvals = c(x, rev(x))
-  yvals = c(y.lo, rev(y.hi))
-  polygon(xvals, yvals, border=NA, col=col)
-}
-
-## plot an example SAR figure for presentation
-par(mfrow=c(1,3))
-site = 'sherman1'
+## plot an example SAR figure for presentation ---------------------------------
+par(mfrow=c(1,1))
+site = 'bigoak'
 purple = rgb(112, 48, 160, maxColorValue=160)
 lightblue = "#1AB2FF"
 
-i = match(site, names(mete))
-    plot(sr ~ area, data=mete[[i]], log='xy',
-         xlim=c(1e1, 1e6), ylim=c(1e1, 1e3),
-         type='n', xlab='', ylab='',frame.plot=F, axes=F,
-         main = site)
-    axis(side=1, at=c(1e1, 1e2, 1e3, 1e4, 1e5, 1e6))
-    axis(side=2, at=c(1e1, 1e2, 1e3))
-    ## mete CI
-#    dat = meteavg[[match(names(mete)[i], names(meteavg))]]
+i = match(site, names(meteEmpirSAD))
+  ## log-log
+    plot(sr_iter ~ area, data=meteEmpirSAD[[i]], 
+         ylim=range(c(meteEmpirSAD[[i]]$sr_iter, empir[[i]]$richness)),
+         xlim=range(c(meteEmpirSAD[[i]]$area, empir[[i]]$area)), log='xy',
+         type='n', main=names(meteEmpirSAD)[i], ylab='SR', xlab='Area (m2)')
+    ## meteEmpirSAD CI
+    dat = meteAvgEmpirSAD[[match(names(meteEmpirSAD)[i], names(meteAvgEmpirSAD))]]
 #    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
-#    lines(sr.avg ~ grains, data=dat, col=lightblue, lwd=4, lty=2)
+    lines(sr.avg ~ grains, data=dat, lwd=3, col='grey')
     ## RP CI
-    dat = srExp[[match(names(mete)[i], names(srExp))]]
-#    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='pink')
-    lines(srCol~ grains, data=dat, type='p', pch=15, col='blue', lwd=4,
-          lty=2, cex=2)
-    ## analytical mete    
-    lines(sr ~ area, data=mete[[i]], type='p', pch=15, col='green3',
-          lwd=4, cex=2)
+    dat = srExp[[match(names(meteEmpirSAD)[i], names(srExp))]]
+#    addCI('grains', 'S_lo', 'S_hi', data='dat', col='pink')
+    lines(S_binom ~ grains, data=dat, col='red', lwd=3)
+    ## analytical meteEmpirSAD    
+    lines(sr_noniter ~ area, data=meteEmpirSAD[[i]], col='dodgerblue', lwd=3)
     ## data
-    lines(richness ~ area, data = empir[[i]], pch=0, type='p',
-          lwd=2, cex=3)
+    lines(richness ~ area, data = empir[[i]], pch=19, type='o', lwd=3)
+    legend('bottomright', c('Empirical','RP','meteEmpirSAD sim', 'meteEmpirSAD noniter'),
+           pch=c(19, rep(NA, 3)), col=c(1, 'red', 'grey', 'dodgerblue'),
+           bty='n', lwd=3)
 
 
-pdf('./figs/mete_&_empir_sar.pdf', width=7 * 2, height=7 * 2)
+
+## Compare METE SAR predictions--------------------------------------------
+pdf('./figs/mete_sar_predictions.pdf', width=7 * 2, height=7 * 2)
   par(mfrow=c(4,4))
-  ## arithmetic
-  for (i in seq_along(mete)) {
-    plot(sr ~ area, data=mete[[i]], 
-         ylim=range(c(mete[[i]]$sr, empir[[i]]$richness)),
-         xlim=range(c(mete[[i]]$area, empir[[i]]$area)),
-         type='n', main=names(mete)[i], xlab='Area (m2)')
-    ## mete CI
-    if (names(mete)[i] != 'cross') {
-      dat = meteavg[[match(names(mete)[i], names(meteavg))]]
+  for (i in seq_along(meteLogSer)) {
+    plot(sr_iter ~ area, data=meteLogSer[[i]], log='xy',
+         ylim=range(c(meteLogSer[[i]]$sr_iter, empir[[i]]$richness)),
+         xlim=range(c(meteLogSer[[i]]$area, empir[[i]]$area)),
+         type='n', main=names(meteLogSer)[i], xlab='Area (m2)')
+    ## Simulated log series
+    if (names(meteLogSer)[i] != 'cross') {
+      dat = meteAvgLogSer[[match(names(meteLogSer)[i], names(meteAvgLogSer))]]
       addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
-      lines(sr.avg ~ grains, data=dat, col='red', type='o')
+    }
+    ## Simulated fixed SAD
+    dat = meteAvgEmpirSAD[[match(names(meteLogSer)[i], names(meteAvgEmpirSAD))]]
+    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='green3')
+    ## Analytical Log series iterative
+    lines(sr_iter ~ area, data=meteLogSer[[i]], type='o', col='red')
+    ## Analytical log series noniterative
+    if (names(meteLogSer)[i] != 'cross')
+      lines(sr_noniter ~ area, data=meteLogSer[[i]], type='o', pch=19, col='blue')
+    ## Analytical fixed SAD iterative
+    lines(sr_iter ~ area, data=meteEmpirSAD[[i]], type='o', col='red', lty=2)
+    ## Analytical fixed SAD noniterative
+    lines(sr_noniter ~ area, data=meteEmpirSAD[[i]], type='o', pch=19, col='blue', lty=2)
+    if (i == 1) {
+      txt = c('Sim CI LogSer', 'Sim CI EmpirSAD', 'Analy iter LogSer', 
+              'Analy iter Empir SAD', 'Analy noniter LogSer', 'Analy noniter EmpirSAD')
+      legend('bottomright', txt, col=c('grey','green3','red','red', 'blue', 'blue'),
+             pch=c(NA, NA,1,1,19,19), lty=c(1,1,1,2,1,2), lwd=c(2,2, rep(1, 4)),
+             bty='n', cex=1)    
+    }  
+  }
+dev.off()
+
+## Compare SAR models and data--------------------------------------------------
+
+## for LogSer SAD
+pdf('./figs/meteLogSer_&_empir_sar.pdf', width=7 * 2, height=7 * 2)
+  par(mfrow=c(4,4))
+  ## log-log
+  for (i in seq_along(meteLogSer)) {
+    plot(sr_iter ~ area, data=meteLogSer[[i]], 
+         ylim=range(c(meteLogSer[[i]]$sr_iter, empir[[i]]$richness)),
+         xlim=range(c(meteLogSer[[i]]$area, empir[[i]]$area)), log='xy',
+         type='n', main=names(meteLogSer)[i], xlab='Area (m2)')
+    ## meteLogSer CI
+    if (names(meteLogSer)[i] != 'cross') {
+      dat = meteAvgLogSer[[match(names(meteLogSer)[i], names(meteAvgLogSer))]]
+      addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
+      lines(sr.avg ~ grains, data=dat, lwd=2, col='darkgrey')
     }  
     ## RP CI
-    dat = srExp[[match(names(mete)[i], names(srExp))]]
-    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='pink')
-    lines(srCol~ grains, data=dat, col='red', type='o')
-    ## analytical mete    
-    lines(sr ~ area, data=mete[[i]], type='o')
+    dat = srExp[[match(names(meteLogSer)[i], names(srExp))]]
+    lines(S_logser_binom ~ grains, data=dat, col='red', lwd=2)
+    ## analytical meteLogSer    
+    lines(sr_iter ~ area, data=meteLogSer[[i]], type='o', col='green3')
+    if (names(meteLogSer)[i] != 'cross') 
+      lines(sr_noniter ~ area, data=meteLogSer[[i]], type='o', col='dodgerblue')
     ## data
     lines(richness ~ area, data = empir[[i]], pch=19, type='o')
     if(i == 1)
-      legend('bottomright',c('Empirical','METE'),pch=c(19,1),bty='n',lty=1)
-  }
-  ## log log
-  for (i in seq_along(mete)) {
-    plot(sr ~ area, data=mete[[i]], log='xy',
-         ylim=range(c(mete[[i]]$sr, empir[[i]]$richness)),
-         xlim=range(c(mete[[i]]$area, empir[[i]]$area)),
-         type='n', main=names(mete)[i], xlab='Area (m2)')
-    if (names(mete)[i] != 'cross') {
-      dat = meteavg[[match(names(mete)[i], names(meteavg))]]
-      addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
-      lines(sr.avg ~ grains, data=dat, col='red', type='o')
-    }
-    ## RP CI
-    dat = srExp[[match(names(mete)[i], names(srExp))]]
-    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='pink')
-    lines(srCol~ grains, data=dat, col='red', type='o')
-    ## analytical mete    
-    lines(sr ~ area, data=mete[[i]], type='o')
-    ## data
-    lines(richness ~ area, data = empir[[i]], pch=19, type='o')
-    if(i == 1)
-      legend('bottomright',c('Empirical','METE'),pch=c(19,1),bty='n',lty=1)
+      legend('bottomright', c('Empirical','RP','meteLogSer sim', 'meteLogSer iter', 'meteLogSer noniter'),
+             pch=c(19, NA, NA, 1, 1), col=c(1, 'red', 'grey', 'green3', 'dodgerblue'),
+             bty='n', lwd=c(2, 3, 3, 2, 2))
   }
 dev.off()    
-  
-## for powerpoint
-## arith
-par(mfrow=c(4,4))
-for (i in seq_along(mete)) {
-  plot(sr ~ area, data = mete[[i]], 
-       ylim=range(c(mete[[i]]$sr, empir[[i]]$richness)),
-       xlim=range(c(mete[[i]]$area, empir[[i]]$area)),
-       type='n', main=names(mete)[i], ylab='',xlab='',axes=F)  
-  axis(side=1, lwd=4, cex.axis=2)
-  axis(side=2, lwd=4, cex.axis=2)
-  if (names(mete)[i] != 'cross') {
-      dat = meteavg[[match(names(mete)[i], names(meteavg))]]
-      addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
-      lines(sr.avg ~ grains, data=dat, col='red', type='o')
-  }      
-  lines(sr ~ area, data = mete[[i]], pch=1, type='o', lwd=3, cex=2)
-  lines(richness ~ area, data = empir[[i]], pch=19, type='o', lwd=2, cex=2)
-}
-## loglog
-par(mfrow=c(4,4))
-for (i in seq_along(mete)) {
-  plot(sr ~ area, data = mete[[i]], log='xy',
-       ylim=range(c(mete[[i]]$sr, empir[[i]]$richness)),
-       xlim=range(c(mete[[i]]$area, empir[[i]]$area)),
-       type='n', main=names(mete)[i], ylab='',xlab='',axes=F)  
-  axis(side=1, lwd=4, cex.axis=2)
-  axis(side=2, lwd=4, cex.axis=2)
-  if (names(mete)[i] != 'cross') {
-      dat = meteavg[[match(names(mete)[i], names(meteavg))]]
-      addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
-      lines(sr.avg ~ grains, data=dat, col='red', type='o')
-  }        
-  lines(sr ~ area, data = mete[[i]], pch=1, type='o', lwd=3, cex=2)
-  lines(richness ~ area, data = empir[[i]], pch=19, type='o', lwd=2, cex=2)
-}
-par(mfrow=c(1,1))
-plot(1:10,1:10,type='n',axes=F,xlab='',ylab='')
-legend('center',c('Empirical','METE'),pch=c(19,1),bty='n',lty=1,lwd=6,cex=3)
 
-## compare residuals between METE and RP
-## create flat file for easy access
+## for EmpirSAD
+pdf('./figs/meteEmpirSAD_&_empir_sar.pdf', width=7 * 2, height=7 * 2)
+  par(mfrow=c(4,4))
+  ## log-log
+  for (i in seq_along(meteEmpirSAD)) {
+    plot(sr_iter ~ area, data=meteEmpirSAD[[i]], 
+         ylim=range(c(meteEmpirSAD[[i]]$sr_iter, empir[[i]]$richness)),
+         xlim=range(c(meteEmpirSAD[[i]]$area, empir[[i]]$area)), log='xy',
+         type='n', main=names(meteEmpirSAD)[i], xlab='Area (m2)')
+    ## meteEmpirSAD CI
+    dat = meteAvgEmpirSAD[[match(names(meteEmpirSAD)[i], names(meteAvgEmpirSAD))]]
+    addCI('grains', 'sr.lo', 'sr.hi', data='dat', col='grey')
+    lines(sr.avg ~ grains, data=dat, lwd=2, col='darkgrey')
+    ## RP CI
+    dat = srExp[[match(names(meteEmpirSAD)[i], names(srExp))]]
+#    addCI('grains', 'S_lo', 'S_hi', data='dat', col='pink')
+    lines(S_binom ~ grains, data=dat, col='red', lwd=2)
+    ## analytical meteEmpirSAD    
+    lines(sr_iter ~ area, data=meteEmpirSAD[[i]], type='o', col='green3')
+    lines(sr_noniter ~ area, data=meteEmpirSAD[[i]], type='o', col='dodgerblue')
+    ## data
+    lines(richness ~ area, data = empir[[i]], pch=19, type='o')
+    if(i == 1)
+      legend('bottomright', c('Empirical','RP','meteEmpirSAD sim', 'meteEmpirSAD iter', 'meteEmpirSAD noniter'),
+             pch=c(19, NA, NA, 1, 1), col=c(1, 'red', 'grey', 'green3', 'dodgerblue'),
+             bty='n', lwd=c(2, 3, 3, 2, 2))
+  }
+dev.off()   
 
-for (i in seq_along(empir)) {
-  site = names(empir)[i]
-  if (site == 'cross')
-    next
-  area = empir[[i]]$area
-  obs.sr = empir[[i]]$richness
-  index = match(site, names(meteavg))
-  mete.sr = meteavg[[index]]$sr.avg
-  mete.res = obs.sr - mete.sr
-  index = match(site, names(srExp))
-  rp.sr = srExp[[index]]$srCol
-  rp.res = obs.sr - rp.sr
-  if (exists('sr_res'))
-    sr_res = rbind(sr_res,
-                   data.frame(site, area, obs.sr, mete.sr, rp.sr, mete.res, rp.res))
-  else
-    sr_res = data.frame(site, area, obs.sr, mete.sr, rp.sr, mete.res, rp.res)
-}
+## compare residuals between METE and RP----------------------------------------
 
-plot(mete.sr ~ obs.sr, data=sr_res)
-points(rp.sr ~ obs.sr, data=sr_res, pch=19)
-abline(a=0, b=1)
+shrtnm = as.character(read.table('./data/shrtnames.txt', colClasses='character'))
+habitat = as.character(read.table('./data/habitat.txt', colClasses='character'))
+hab = c('tropical', 'oak-hickory', 'pine', 'oak savanna', 'mixed evergreen',
+        'grassland')
+col = c("forestgreen", "#1AB2FF", "medium purple", "#E61A33", "#B2FF8C",
+        "#FF8000")
 
-plot(mete.res ~ rp.res, data=sr_res)
-abline(a=0, b=1)
+as.matrix(sort(apply(abs(sar_res[ , 3:10]), 2, mean, na.rm=T)))
+as.matrix(sort(apply(abs(sar_res[!is.na(sar_res[,4]), 3:10]), 2, mean, na.rm=T)))
 
-sites = unique(sr_res$site)
-plot(mete.res ~ area, data=sr_res, log='x', ylim=c(-40, 40), 
-     type='n', frame.plot=F, axes=F, xlab='', ylab='',
-     xlim = c(0.1, 1e6))
-axis(side=1, cex.axis=1.75, padj=.5, lwd=8,
-     at=10 ^ (-1:6))
-axis(side=2, cex.axis=1.75, lwd=8)
-abline(h=0, lwd=5)
-for (i in seq_along(sites)) {
-  habindex = match(habitat[match(sites[i], shrtnm)], hab)
-  lines(mete.res ~ area, data=sr_res, subset= site == sites[i],
-        lwd=4, col=habcol[habindex])
-  lines(rp.res ~ area, data=sr_res, subset= site == sites[i],
-        lwd=4, col=habcol[habindex], lty=2)
-}
+## average residuals on a per site basis
+## then examine which model performs the best for each site
+## this keeps sites with lots of spatial scales from dominating
+## the residuals
 
-##----------------------------------------------------------------------------
-## plot universal curve
+avg_sar_res = aggregate(sar_res[ , -(1:2)], by=list(sar_res$site), 
+                        FUN = function(x) mean(x^2, na.rm=T))
+indices = apply(avg_sar_res[ , -1], 1, function(x) which(min(x, na.rm=T) == x))
+wins = as.matrix(table(names(avg_sar_res[ , -1])[c(indices,1:8)]) - 1)
+res_avg = apply(avg_sar_res[ , -1], 2, mean, na.rm=T)
+cbind(wins, res_avg)
+                   res_avg
+empirsad_avg     0 44.68045
+empirsad_iter    0 59.86338
+empirsad_noniter 6 27.78716
+empirsad_rp      1 13.26835
+logser_avg       3 15.75582
+logser_iter      2 44.67802
+logser_noniter   2  3.40553
+logser_rp        2 28.08008
+
+## compare only at the empirical SAD models
+avg_sar_res = aggregate(sar_res[ , -(1:6)], by=list(sar_res$site), 
+                        FUN = function(x) mean(x^2, na.rm=T))
+indices = apply(avg_sar_res[ , -1], 1, function(x) which(min(x) == x))
+wins = as.matrix(table(names(avg_sar_res[ , -1])[c(indices,1:4)]) - 1)
+res_avg = apply(avg_sar_res[ , -1], 2, mean)
+cbind(wins, res_avg)
+                     res_avg
+empirsad_avg      4 15.75582
+empirsad_iter     0 44.67802
+empirsad_noniter 10  3.40553
+empirsad_rp       2 28.08008
+
+## compare only at the empirical SAD models simulated iterative and rp
+avg_sar_res = aggregate(sar_res[ , c('empirsad_avg','empirsad_rp')],
+                        by=list(sar_res$site), 
+                        FUN = function(x) mean(x^2, na.rm=T))
+indices = apply(avg_sar_res[ , -1], 1, function(x) which(min(x) == x))
+wins = as.matrix(table(names(avg_sar_res[ , -1])[c(indices,1:4)]) - 1)
+res_avg = apply(avg_sar_res[ , -1], 2, mean)
+cbind(wins, res_avg)
+                 res_avg
+empirsad_avg 12 15.75582
+empirsad_rp   4 28.08008
+
+## plot residuals
+pdf('./figs/sar_residuals.pdf', width=7 * 3, height=7)
+  sites = unique(sar_res$site)
+  par(mfrow=c(1,3))
+  plot(empirsad_noniter ~ area, data=sar_res, log='x', ylim=c(-25, 25), 
+       type='n', frame.plot=F, axes=F, xlab='', ylab='',
+       xlim = c(0.1, 1e6), main='METE noniter')
+  axis(side=1, cex.axis=1.75, padj=.5, lwd=8, at=10 ^ (-1:6))
+  axis(side=2, cex.axis=1.75, lwd=8)
+  abline(h=0, lwd=5)
+  for (i in seq_along(sites)) {
+    habindex = match(habitat[match(sites[i], shrtnm)], hab)
+    lines(empirsad_noniter ~ area, data=sar_res, subset= site == sites[i],
+          lwd=4, col=col[habindex])
+  }
+  legend('bottomright', hab, col=col, lwd=2, bty='n')
+  plot(empirsad_noniter ~ area, data=sar_res, log='x', ylim=c(-25, 25), 
+       type='n', frame.plot=F, axes=F, xlab='', ylab='',
+       xlim = c(0.1, 1e6), main='METE iter sim')
+  axis(side=1, cex.axis=1.75, padj=.5, lwd=8, at=10 ^ (-1:6))
+  axis(side=2, cex.axis=1.75, lwd=8)
+  abline(h=0, lwd=5)
+  for (i in seq_along(sites)) {
+    habindex = match(habitat[match(sites[i], shrtnm)], hab)
+    lines(empirsad_avg ~ area, data=sar_res, subset= site == sites[i],
+          lwd=4, col=col[habindex])
+  }
+  plot(empirsad_noniter ~ area, data=sar_res, log='x', ylim=c(-25, 25), 
+       type='n', frame.plot=F, axes=F, xlab='', ylab='',
+       xlim = c(0.1, 1e6), main='RP')
+  axis(side=1, cex.axis=1.75, padj=.5, lwd=8, at=10 ^ (-1:6))
+  axis(side=2, cex.axis=1.75, lwd=8)
+  abline(h=0, lwd=5)
+  for (i in seq_along(sites)) {
+    habindex = match(habitat[match(sites[i], shrtnm)], hab)
+    lines(empirsad_rp ~ area, data=sar_res, subset= site == sites[i],
+          lwd=4, col=col[habindex], lty=1)
+  }
+dev.off()
+
+## plot universal curve --------------------------------------------------------
 
 slopes = read.csv('./sar/sar_slopes.csv')
 slopes = data.frame(slopes,logNS = log(slopes$NS))
@@ -264,12 +277,6 @@ pdf('./figs/universal_sar.pdf', width=7 * 1.25, height=7)
 dev.off()
 
 ## for slide
-shrtnm = as.character(read.table('./data/shrtnames.txt', colClasses='character'))
-habitat = as.character(read.table('./data/habitat.txt', colClasses='character'))
-hab = c('tropical', 'oak-hickory', 'pine', 'oak savanna', 'mixed evergreen',
-        'grassland')
-col = c("forestgreen", "#1AB2FF", "medium purple", "#E61A33", "#B2FF8C",
-        "#FF8000")
 pch = c(17, 0, 16, 1, 15, 2)
 pch = rep(19, 6)
 
