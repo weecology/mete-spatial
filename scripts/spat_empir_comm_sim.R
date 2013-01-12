a35d3bb3f22fa729adb092b88a255fc05abd76f3
Dan McGlinn <danmcglinn@gmail.com>
2013-01-12 03:51:19 +0000

    Simulates communities parameterized from the empirical communities. Intended to replace spat_sim_comms.sh

---
+++ scripts/spat_empir_comm_sim.R
@@ -0,0 +1,35 @@
+## Purpose: to simulate communities that are parameterized by the empirically 
+## observed communities.
+
+setwd('~/maxent/spat/scripts')
+
+clArgs = commandArgs(trailingOnly=TRUE)
+server = clArgs[1]
+indices=clArgs[2]
+
+names = read.table('../data/shrtnames.txt', colClasses='character')
+bisect = read.table('../data/bisect_fine.txt')
+S = read.table('../data/S_vals.txt') 
+N = read.table('../data/N_vals.txt') 
+
+sadType = c("meteSAD", "empirSAD")
+
+for (i in indices) {
+  for (j in sadType) {
+    log_file = paste('./log_files/error_sim_analysis_', names[i],
+                     '_', j, '.log', sep='')
+    if (j == 'meteSAD')
+      abu_file = 'None'
+    else
+      abu_file = paste('../data/', names[i], '_sad.csv', sep='')
+    if (server == 'unc')
+      system(paste('bsub -q week -M 8 -J' names[i], '-o'
+                   log_file, 'python spat_community_generation.py',
+                   S[i], N[i], 200, bisect[i], 'False', 'None',
+                   names[i], sep=' '))
+    else
+      system(paste('python spat_community_generation.py',
+                   S[i], N[i], 200, bisect[i], 'False', 'None',
+                   names[i], '>', log_file, '2>&1 &', sep=' '))
+  }
+}
