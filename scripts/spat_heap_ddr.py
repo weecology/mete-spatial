2223f52178eec0d131b3fc08607ef3579cca8e25
Dan McGlinn <danmcglinn@gmail.com>
2013-01-14 06:17:36 +0000

    Added ability to compute the analytical ddr for either the fixed SAD or the inferred truncated logseries using S0 and N0.

--- scripts/spat_heap_ddr.py
+++ scripts/spat_heap_ddr.py
@@ -12,17 +12,18 @@ if len(sys.argv) > 1:
     A = int(sys.argv[1]) 
     A0 = int(sys.argv[2])
     shape = sys.argv[3]
-    abu_filename = sys.argv[4]
-    out_filename = sys.argv[5]
-    if len(sys.argv) == 7:
+    sadType = sys.argv[4]
+    abu_filepath = sys.argv[5]
+    out_filepath = sys.argv[6]
+    if len(sys.argv) == 8:
         unit_distance = float(sys.argv[6])
     else:
         unit_distance = 1
 else:
     print 'Error: Specify A, A0, shape, and abu file at the command line'
 
-if abu_filename  != 'None':
-    datafile = open(abu_filename, 'r')
+if abu_filepath  != 'None':
+    datafile = open(abu_filepath, 'r')
     datareader = csv.reader(datafile)
     data = []
     for row in datareader:
@@ -31,9 +32,14 @@ if abu_filename  != 'None':
 else:
     print 'Error: Abundance data not specified'
 
-sor = mete.sor_heap(A, abu, A0, shape, unit_distance)
+if sadType == 'meteSAD':
+    S0 = len(abu)
+    N0 = sum(abu)
+    sor = mete.sor_heap(A, A0, S0, N0, shape, unit_distance)
+else:
+    sor = mete.sor_heap_fixed_abu(A, abu, A0, shape, unit_distance)
 
-writer = open(out_filename, 'wb') 
+writer = open(out_filepath, 'wb') 
 datawriter = csv.writer(writer)
  
 datawriter.writerow(['j', 'dist', 'sor'])
