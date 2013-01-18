d943dc5264bce75ea286cc28d495accbd060d3e2
Dan McGlinn <danmcglinn@gmail.com>
2013-01-18 23:15:38 +0000

    Updated so that psi is accepted as an input from the command line

--- scripts/spat_bisect_ddr.py
+++ scripts/spat_bisect_ddr.py
@@ -9,17 +9,18 @@ import csv
 import sys
 
 if len(sys.argv) > 1:
-    bisect_fine = int(sys.argv[1]) 
-    bisect_coarse = int(sys.argv[2])
-    sadType = sys.argv[3]
-    abu_filepath = sys.argv[4]
-    out_filepath = sys.argv[5]
-    if len(sys.argv) == 7:
-        unit_distance = float(sys.argv[6])
+    psi = float(sys.argv[1])
+    bisect_fine = int(sys.argv[2]) 
+    bisect_coarse = int(sys.argv[3])
+    sadType = sys.argv[4]
+    abu_filepath = sys.argv[5]
+    out_filepath = sys.argv[6]
+    if len(sys.argv) == 8:
+        unit_distance = float(sys.argv[7])
     else:
         unit_distance = 1
 else:
-    print 'Error: Specify bisect_fine, bisect_coarse, sadType,  abu file path, and out filepath at the command line'
+    print 'Error: Specify psi, bisect_fine, bisect_coarse, sadType, abu file path, and out filepath at the command line'
 
 if abu_filepath  != 'None':
     datafile = open(abu_filepath, 'r')
@@ -39,16 +40,16 @@ shape = 'golden' # so that results for every j value are calculated
 if sadType == 'meteSAD':
     S0 = len(abu)
     N0 = sum(abu)
-    sor = [mete.sor_heap(A, A0, S0, N0, shape, unit_distance) for A in A_vals]
+    sor = [mete.sor_bisect(A, A0, S0, N0, psi, shape, unit_distance) for A in A_vals]
     sor = np.concatenate((sor), axis=0)
 else:
-    sor = [mete.sor_heap_fixed_abu(A, abu, A0, shape, unit_distance) for A in A_vals]
+    sor = [mete.sor_bisect_fixed_abu(A, abu, A0, psi, shape, unit_distance) for A in A_vals]
     sor = np.concatenate((sor), axis=0)
 
 writer = open(out_filepath, 'wb') 
 datawriter = csv.writer(writer)
  
-datawriter.writerow(['i', 'j', 'dist', 'sor'])
+datawriter.writerow(['psi', 'i', 'j', 'dist', 'sor'])
 for i in range(0, np.shape(sor)[0]):
     datawriter.writerow(sor[i, ])
      
