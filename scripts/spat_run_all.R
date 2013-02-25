fe00068f66e665121b595a3b654a617740ea4ee3
Daniel McGlinn <danmcglinn@gmail.com>
2013-02-25 11:45:15 -0700

    Added wait statements to the system operations as well as additional annotation to the code

--- scripts/spat_run_all.R
+++ scripts/spat_run_all.R
@@ -10,51 +10,60 @@ server = 'unc'
 
 ## empirical data analysis ------------------------------------------------
 
-## filter the empirical data
-system('Rscript spat_empir_data_filtering.R')
+## filter the empirical data (quick)
+system('Rscript spat_empir_data_filtering.R', wait=TRUE)
 
-## summarize the empirical data
-system('Rscript spat_empir_data_summary.R')
+## summarize the empirical data (quick)
+system('Rscript spat_empir_data_summary.R', wait=TRUE)
 
-## simulate the empirical data
+## simulate the empirical data (slow)
 indices = paste(1:19, collapse=' ')
 system(paste('Rscript spat_empir_comm_sim.R', server,
-             "'", indices, "'", sep=" "))
+             "'", indices, "'", sep=" "), wait=FALSE)
 
-## analyze emprical observed DDR 
+## analyze emprical observed DDR (very slow)
 npar = 8
 nperm = 500
 commName = as.character(read.table('../data/shrtnames.txt', colClasses='character'))
 commName = paste("'", paste(commName, collapse=' '), "'", sep='')
 system(paste("Rscript spat_empir_observed_ddr.R", server,
-             npar, nperm, commName, sep=" "))
+             npar, nperm, commName, sep=" "), wait=FALSE)
 
-## analyze emprical simulated DDR
+## analyze emprical simulated DDR (very slow)
 indices = paste("'", paste(1:19, collapse=' '), "'", sep='')
 system(paste("Rscript spat_empir_simulated_ddr.R", server,
-             indices, sep=" "))
+             indices, sep=" "), wait=FALSE)
 
-## analyze empirical observed SAR 
-system('Rscript spat_empir_sar.R')
+## generate analytical mete DDR expectation for the empirical communities
 
-## analyze empirical simulated SAR
+system(paste('Rscript spat_empir_analytical_ddr.R', server,
+             site_index, sadType, sep=' '), wait=FALSE)
+
+## analyze empirical observed SAR (3 min)
+system('Rscript spat_empir_sar.R', wait=FALSE)
+
+## analyze empirical simulated SAR 
 sitename = as.character(read.table('../data/shrtnames.txt', colClasses='character'))
-sitename = paste("'", sitename, "'", sep='')
-system(paste('Rscript spat_mete_sar_avgs.R', sitename, sep=' '))
+for (i in sitename)
+  system(paste('Rscript spat_mete_sar_avgs.R', i, sep=' '),
+         wait=FALSE)
 
 ## generate binomial expected SAR 
-system('Rscript spat_empir_expected_sars.R')
+system('Rscript spat_empir_expected_sars.R', wait=FALSE)
 
 ## generate METE expected SAR
 system('chmod u+x spat_mete_sar.sh')
 indices = paste("'", paste(1:19, collapse=' '), "'", sep='')
 system(paste('./spat_mete_sar.sh', server, indices, sep=' '), wait=FALSE)
+## need to update spat_mete_sar.py so that it computes the results
+## for both the inferred and empirical SAD
+
 
 ## parameter space analysis -----------------------------------------------
 ## simulate communities across parameter space
-system('python gencomms.py')
+system('python gencomms.py', wait=FALSE)
 
 ## analyze parameter space communities DDR
 
 ## analyze parameter space communities SAR
-system('Rscript spat_param_sar_avgs.R')
+system('Rscript spat_param_sar_avgs.R', wait=FALSE)
