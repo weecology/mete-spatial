f0e3e563e8d91361f4c7a37506148ab979f38746
Daniel McGlinn <danmcglinn@gmail.com>
2013-04-01 15:33:43 -0600

    Add for loop to automate the computation of DDR for all sites

--- scripts/spat_mete_ddr.R
+++ scripts/spat_mete_ddr.R
@@ -17,31 +17,33 @@ grain_fine = as.numeric(read.table('./data/grain_fine.txt'))
 bisect_fine = as.numeric(read.table('./data/bisect_fine.txt'))
 bisect_coarse = as.numeric(read.table('./data/bisect_coarse.txt'))
 
-i = site_index
+sadType = ifelse(sadType == 'both', c('empirSAD','meteSAD'), sadType)
 
-unit_distance = sqrt(grain_fine[i]) * n_pixels_wide(bisect_fine[i])
-abu_file = paste('./data/', shrtnames[i], '_sad.csv', sep='')
-log_file = paste('./scripts/log_files/', shrtnames[i], '_mete_sor.log', sep='')
-
-if (sadType == 'meteSAD')
-  out_file = paste('./sorensen/', shrtnames[i], 
-                   '_mete_sor.csv', sep='')
-if (sadType == 'empirSAD')
-  out_file = paste('./sorensen/', shrtnames[i], 
-                   '_empirSAD_mete_sor.csv', sep='')
-
-if (server == 'unc')
-  cmd = paste('bsub -q week -x -o', log_file, '-J', shrtnames[i],
-              'python ./scripts/spat_heap_ddr.py',
-              bisect_fine[i], bisect_coarse[i], 
-              sadType, abu_file, out_file, 
-              unit_distance, sep=' ')
-if (server == 'usu')
-  cmd = paste('nice python ./scripts/spat_heap_ddr.py',
-              bisect_fine[i], bisect_coarse[i],
-              sadType, abu_file, out_file, 
-              unit_distance, '>', log_file, '2>&1', sep=' ')
-
-system(cmd, wait=FALSE)
-
-
+for(i in site_index) {
+  for(j in sadType) {
+    unit_distance = sqrt(grain_fine[i]) * n_pixels_wide(bisect_fine[i])
+    abu_file = paste('./data/', shrtnames[i], '_sad.csv', sep='')
+    log_file = paste('./scripts/log_files/', shrtnames[i], '_mete_sor.log', sep='')
+    
+    if (j == 'meteSAD')
+      out_file = paste('./sorensen/', shrtnames[i], 
+                       '_mete_sor.csv', sep='')
+    if (j == 'empirSAD')
+      out_file = paste('./sorensen/', shrtnames[i], 
+                       '_empirSAD_mete_sor.csv', sep='')
+    
+    if (server == 'unc')
+      cmd = paste('bsub -q week -x -o', log_file, '-J', shrtnames[i],
+                  'python ./scripts/spat_heap_ddr.py',
+                  bisect_fine[i], bisect_coarse[i], 
+                  j, abu_file, out_file, 
+                  unit_distance, sep=' ')
+    if (server == 'usu')
+      cmd = paste('nice python ./scripts/spat_heap_ddr.py',
+                  bisect_fine[i], bisect_coarse[i],
+                  j, abu_file, out_file, 
+                  unit_distance, '>', log_file, '2>&1', sep=' ')
+    
+    system(cmd, wait=FALSE)
+  }  
+}
