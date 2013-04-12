ce607f594568ca283efb415460567e5697ed1869
Daniel McGlinn <danmcglinn@gmail.com>
2013-04-12 10:56:28 -0600

    Compute bisection variograms rather than traditional variograms.

--- scripts/spat_empir_analysis.R
+++ scripts/spat_empir_analysis.R
@@ -5,7 +5,7 @@
 setwd('~/maxent/spat')
 
 library(vegan)
-library(danspkg)
+#library(danspkg)
 source('./scripts/spat_sim_vario_func.R')
 
 clArgs = commandArgs(trailingOnly=TRUE)
@@ -13,37 +13,36 @@ if (length(clArgs) > 1) {
   commName = clArgs[1]
   metricsToCalc = clArgs[2]
   dataType = clArgs[3]
-  nperm = as.numeric(clArgs[4])
+  method = clArgs[4]
   npar = as.numeric(clArgs[5])
+  nperm = as.numeric(clArg[6])
 }
 if (length(clArgs) == 0) {
   stop('Must specify commName, metricsToCalc, dataType, nperm, and npar at
        command line')
   q('no')
 }
-if (is.na(nperm)) {
-  nperm = NULL
-  npar = 1
+if (method == 'univariate') {
+  univariate = TRUE
+}
+if (method == 'multivariate') {
+  univariate = FALSE
+}
+if (npar > 1) {
+  library(snowfall)
+  library(rlecuyer)
+  sfInit(parallel=TRUE, cpus=npar, type="SOCK")
 }
 
 ## read in comms file 
 comms = read.csv(paste('./data/',commName, '_comms.csv', sep=''))
 
-## specify how to bin the spatial lags
-spat_breaks = read.csv('./data/nbreaks.csv')
-spat_breaks$nbreaks = spat_breaks$nbreaks + 1
-if (any(spat_breaks$comm == commName)) {
-  breaks = spat_breaks$nbreaks[spat_breaks$comm == commName]
-  log = TRUE
-}
-if (!any(spat_breaks$comm == commName)) {
-  breaks = NA
-}
-
 ## specify quantiles to examine variogram at
 quants = c(0.25, 0.50, 0.75)
 
 ## compute Dist Decay statistics
-metrics = calcMetrics(comms, metricsToCalc, dataType, breaks=breaks, 
-                      log=log, quants=quants, nperm=nperm, npar=npar,
-                      writeToFile=TRUE, fileSuffix=commName)
+metrics = calc_metrics_bisect(comms, metricsToCalc, dataType, quants,
+                              nperm, univariate, writeToFile=F,
+                              fileSuffix=commName)
+
+sfStop()
