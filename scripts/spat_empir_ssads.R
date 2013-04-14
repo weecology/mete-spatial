3d7335e64ab13110b725a79c7c7111d41f5a0b24
Daniel McGlinn <danmcglinn@gmail.com>
2013-04-14 10:53:42 -0600

    Script for creating empirical spatial species abundance distributions

---
+++ scripts/spat_empir_ssads.R
@@ -0,0 +1,22 @@
+setwd('~/maxent/spat')
+source('./scripts/spat_sim_vario_func.R')
+
+## read in empirical data---------------------------------
+fileNames = dir('./data')
+commFiles = grep('comms', fileNames)
+dat = vector('list', length(commFiles))
+names(dat) = sub('_comms.csv', '', fileNames[commFiles])
+for (i in seq_along(commFiles))
+  dat[[i]] = as.matrix(read.csv(paste('./data/', fileNames[commFiles[i]], sep='')))
+
+## compute SSAD at each spatial scale---------------------
+ssads = sapply(dat, get_SSAD)
+
+## export SSADs as .csv files-----------------------------
+for (i in seq_along(dat))
+  write.csv(ssads[[i]], 
+            file=paste('./data/', names(dat)[i], '_ssads.csv',sep=''), 
+            row.names=F)
+
+
+  
