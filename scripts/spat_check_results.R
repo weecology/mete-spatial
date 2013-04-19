d74c57065096f60302ca8f219f8ca393a44abadb
Daniel McGlinn <danmcglinn@gmail.com>
2013-04-18 23:06:05 -0600

    Fix file name filtering mistake, update what jobs remain to be run.

--- scripts/spat_check_results.R
+++ scripts/spat_check_results.R
@@ -31,24 +31,23 @@ sites[!sites %in% tmp_sites]
 ## bisected abundance empirical results
 
 tmp = files[grep('_bisect_abu.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp, invert=TRUE)]
+tmp = tmp[grep('_C200_', tmp, invert=TRUE)]
 tmp = tmp[grep('_uni_', tmp, invert=TRUE)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites] 
-## sherman1, sherman2, luquillo, ferp on killdevil
-## landsend on jayne 
-## bryan on wash
+## sherman1, luquillo, ferp on killdevil
 ## bigoak on zoe
 
 dat = load_list(tmp, TRUE, 'sorensen')
-for(i in seq_along(dat)) { 
-  print(paste(tmp[i], ncol(dat[[1]][[1]]$sorensenNull$vario)))
+for(i in seq_along(dat)) {
+  for(j in seq_along(dat[[i]]))
+    print(paste(tmp[i], ncol(dat[[i]][[j]]$sorensenNull$vario)))
 }
 
 
 ## bisected abundance univariate empirical results
 tmp = files[grep('_bisect_abu.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp, invert=TRUE)]
+tmp = tmp[grep('_C200_', tmp, invert=TRUE)]
 tmp = tmp[grep('_uni_', tmp)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites] 
@@ -67,33 +66,33 @@ sites[!sites %in% tmp_sites]
 
 ## bisected binary logseries simulated results
 tmp = files[grep('_bisect_binary.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp)]
+tmp = tmp[grep('_C200_', tmp)]
 tmp = tmp[grep('_empirSAD_', tmp, invert=T)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites] 
 
 dat = load_list(tmp, TRUE, 'metrics')
 for(i in seq_along(dat)) { 
-  print(paste(tmp[i], sum(sapply(dat[[1]], function(x) !is.null(x)))))
+  print(paste(tmp[i], sum(sapply(dat[[i]], function(x) !is.null(x)))))
 }
 
 ## bisected abundance logseries simulated results
 tmp = files[grep('_bisect_abu.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp)]
+tmp = tmp[grep('_C200_', tmp)]
 tmp = tmp[grep('_empirSAD_', tmp, invert=T)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites] 
 
 ## bisected binary empirSAD simulated results
 tmp = files[grep('_bisect_binary.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp)]
+tmp = tmp[grep('_C200_', tmp)]
 tmp = tmp[grep('_empirSAD_', tmp)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites] 
 
 ## bisected abundance empirSAD simulated results
 tmp = files[grep('_bisect_abu.Rdata', files)]
-tmp = tmp[grep('_C200_B13_', tmp)]
+tmp = tmp[grep('_C200_', tmp)]
 tmp = tmp[grep('_empirSAD_', tmp)]
 tmp_sites = as.character(sapply(tmp, function(x) strsplit(x, '_')[[1]][2]))
 sites[!sites %in% tmp_sites]
