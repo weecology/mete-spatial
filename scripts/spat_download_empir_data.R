3fe4ac2b2421e483ebd8eaff9361f027a5fb1fb2
Daniel McGlinn <danmcglinn@gmail.com>
2013-04-29 16:45:15 -0600

    Script for downloading the publicly available datasets

---
+++ scripts/spat_download_empir_data.R
@@ -0,0 +1,24 @@
+
+setwd('~/maxent/spat')
+
+library(RCurl)
+
+urls = read.csv('./public_data_urls.csv', colClasses='character')
+
+download_data = function(urls, output_path) {
+  for (i in seq_along(urls)) {
+    temp = getURL(urls[i])
+    dat = read.csv(textConnection(temp))
+    namesplit = unlist(strsplit(urls[i], split='/'))
+    filename = namesplit[length(namesplit)]
+    write.csv(dat, file = file.path(path, filename), row.names=FALSE)
+    print(paste(filename, ' was downloaded and written to ',
+                 file.path(path, filename), sep=''))
+  }
+}
+
+path = './data/raw_data'
+
+download_data(urls$url, './data/raw_data')
+
+
