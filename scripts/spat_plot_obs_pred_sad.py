be6c99085f13c71ef34c1d7e4cc1bdc969f8af92
dmcglinn <danmcglinn@gmail.com>
2013-04-29 00:39:40 -0600

    Script creates graphics for the SAD one-to-one relationships

---
+++ scripts/spat_plot_obs_pred_sad.py
@@ -0,0 +1,14 @@
+from working_functions import *
+
+datasets = ['bci','cocoli1','cocoli2','cross','sherman1','sherman2',
+            'sherman3', 'serp', 'oosting', 'ferp', 'luquillo', 'graveyard',
+            'landsend', 'rocky', 'bormann', 'woodbridge', 'baldmnt', 'bryan',
+            'bigoak']
+
+plot_obs_pred_sad(datasets, data_dir='../sad/',
+                  dest_dir='../figs/sad_figs/')
+
+for shrt_name in datasets:
+    plot_obs_pred_sad([shrt_name], data_dir='../sad/', 
+                      dest_dir='../figs/sad_figs/' + shrt_name + '_')
+    
