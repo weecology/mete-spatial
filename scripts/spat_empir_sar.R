43949f214ad52b9a663af023add7f50b1ac1bae1
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-08 01:04:57 -0600

    add missing comma

--- scripts/spat_empir_sar.R
+++ scripts/spat_empir_sar.R
@@ -34,7 +34,7 @@ Ms = n_pixels_wide(bisect_fine)
 psp = vector('list',length(dat))
 names(psp) = names(dat)
 for(i in seq_along(dat))
-  psp[[i]] = mat2psp(dat[[i]][ , -(1:3)], dat[[i]][ , 2:3]
+  psp[[i]] = mat2psp(dat[[i]][ , -(1:3)], dat[[i]][ , 2:3],
                      Ns[i], Ms[i])
 
 ## compute SARs, non-movinging window 
