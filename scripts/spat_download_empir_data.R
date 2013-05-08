3bc7da8eb361ea537d9398a12bbfc402f4e3b00d
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-08 00:30:07 -0600

    Add http for Oosting site and a field for the data delimiter. Correct error in specification of path in download_data function

--- scripts/spat_download_empir_data.R
+++ scripts/spat_download_empir_data.R
@@ -5,18 +5,25 @@ library(RCurl)
 
 urls = read.csv('./public_data_urls.csv', colClasses='character')
 
-download_data = function(urls, output_path) {
+download_data = function(urls, delim, output_path) {
   for (i in seq_along(urls)) {
     temp = getURL(urls[i])
-    dat = read.csv(textConnection(temp))
+    if (delim[i] == 'comma')
+      dat = read.csv(textConnection(temp))
+    if (delim[i] == 'tab')
+      dat = read.delim(textConnection(temp))
     namesplit = unlist(strsplit(urls[i], split='/'))
     filename = namesplit[length(namesplit)]
-    write.csv(dat, file = file.path(path, filename), row.names=FALSE)
+    if (delim[i] == 'comma')
+      write.csv(dat, file = file.path(output_path, filename), row.names=FALSE)
+    if (delim[i] == 'tab')
+      write.table(dat, file = file.path(output_path, filename), sep='\t',
+                  row.names=FALSE)
     print(paste(filename, ' was downloaded and written to ',
-                 file.path(path, filename), sep=''))
+                 file.path(output_path, filename), sep=''))
   }
 }
 
-download_data(urls$url, './data/raw_data')
+download_data(urls$url, urls$delim, './data/raw_data')
 
 
