81fba21fcb3a72c956f53c2d055dc257ffa353d6
Daniel McGlinn <danmcglinn@gmail.com>
2013-05-14 16:22:00 -0600

    Deleted system.time() calls

--- scripts/sar_run_all.R
+++ scripts/sar_run_all.R
@@ -25,13 +25,13 @@ system('Rscript spat_empir_data_summary.R', wait=TRUE)
 ## data analysis ---------------------------------------------------------
 
 ## analyze empirical observed SAR (approx 3 min)
-system.time(system('Rscript spat_empir_sar.R', wait=TRUE))
+system('Rscript spat_empir_sar.R', wait=TRUE)
 
 ## generate METE expected SAR (several hours to several days)
 system('python spat_mete_sar.py >./log_files/mete_sar.log 2>&1', wait=FALSE)
 
 ## generate binomial expected SAR (approx 1.5 min)
-system.time(system('Rscript spat_empir_expected_sars.R', wait=TRUE))
+system('Rscript spat_empir_expected_sars.R', wait=TRUE)
 
 ## aggregated results and generate figures ------------------------------
 system('Rscript spat_sar_load_and_avg_data.R', wait=TRUE)
