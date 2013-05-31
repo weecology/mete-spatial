8f6cb8853ca332abaa5e3787c72238acba1478bf
Daniel <danmcglinn@gmail.com>
2013-05-31 17:10:35 -0600

    Update so that it installs R dependencies and runs additional scripts

--- scripts/sar_run_all.R
+++ scripts/sar_run_all.R
@@ -1,8 +1,7 @@
 
 setwd('~/maxent/spat')
-dir.create('./data/')
-dir.create('./sar')
-dir.create('./figs')
+
+install.packages(c('RCurl', 'vegan'))
 
 ## ToDo: make it so that the sitenames are supplied once and 
 ## not within each individual script
@@ -13,8 +12,14 @@ setwd('./scripts')
 ## requires R package: RCurl
 system('Rscript spat_download_empir_data.R')
 
+## get names of datasets
+system('Rscript spat_get_sitenames.R')
+
 ## filter the empirical data 
 system('Rscript spat_empir_data_filtering.R')
+
+## create multiscale site x species matrics (several minutes to several hours)
+system('Rscript spat_empir_gen_comm_mat.R')
 
 ## calculate empirical SAD
 system('Rscript spat_empir_sad.R')
@@ -30,7 +35,7 @@ system('python spat_obs_pred_sad.py')
 ## analyze empirical observed SAR (approx 3 min)
 system('Rscript spat_empir_sar.R')
 
-## generate METE expected SAR (several hours to several days)
+## generate METE expected SAR (several minutes to several days)
 system('python spat_mete_sar.py')
 
 ## generate binomial expected SAR (approx 1.5 min)
@@ -39,6 +44,5 @@ system('Rscript spat_empir_expected_sars.R')
 ## aggregated results and generate figures ------------------------------
 system('Rscript spat_sar_load_and_avg_data.R')
 
-system('Rscript sar_figures.R')
+system('Rscript sar_figures_generic.R')
 
-system('python spat_plot_obs_pred_sad.py')
