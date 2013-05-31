5091552f6942ccfe854bdf87f205cfa0fff48505
Daniel <danmcglinn@gmail.com>
2013-05-31 17:10:00 -0600

    Add download_data() function

--- scripts/spat_functions.R
+++ scripts/spat_functions.R
@@ -2849,3 +2849,29 @@ match_index = function(x, table) {
   indices = 1:len_x
   return(indices[match(x, table, nomatch=0) == 1])
 }
+
+download_data = function(urls, delim, output_path) {
+  ## Function that downloads flat data files from the web
+  ## Arguments:
+  ## urls: the web address of the flat data file
+  ## delim: the column delimintors for each dataset, either
+  ##   'comma' or 'tab'
+  ## output_path: the directory that the data will be written to
+  require(RCurl)
+  for (i in seq_along(urls)) {
+    temp = getURL(urls[i])
+    if (delim[i] == 'comma')
+      dat = read.csv(textConnection(temp))
+    if (delim[i] == 'tab')
+      dat = read.delim(textConnection(temp))
+    namesplit = unlist(strsplit(urls[i], split='/'))
+    filename = namesplit[length(namesplit)]
+    if (delim[i] == 'comma')
+      write.csv(dat, file = file.path(output_path, filename), row.names=FALSE)
+    if (delim[i] == 'tab')
+      write.table(dat, file = file.path(output_path, filename), sep='\t',
+                  row.names=FALSE)
+    print(paste(filename, ' was downloaded and written to ',
+                file.path(output_path, filename), sep=''))
+  }
+}
