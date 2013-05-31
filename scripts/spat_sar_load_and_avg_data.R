6d62f4c7b9ccaa03c6b375800fa6c711791d417f
Daniel <danmcglinn@gmail.com>
2013-05-31 17:17:05 -0600

    Drop simulated SAR averaging. Only run on local datasets.

--- scripts/spat_sar_load_and_avg_data.R
+++ scripts/spat_sar_load_and_avg_data.R
@@ -1,7 +1,8 @@
 ## Loads and averages the species area anaylsis data products
 
 setwd('~/maxent/spat')
-source('./scripts/spat_sim_vario_func.R')
+
+source('./scripts/spat_functions.R')
 
 print('Aggregating SAR results, ...')
 
@@ -31,63 +32,43 @@ for (i in seq_along(meteEmpirSADFiles)) {
   meteEmpirSAD[[i]]$area = meteEmpirSAD[[i]]$area * empir[[i]]$area[1]
 }
 
-## mete SARs averaged Simulated
-meteAvgFiles = grep('mete_sar_avgs.csv', fileNames, value=TRUE)
-meteAvgEmpirSADFiles = grep('empirSAD', meteAvgFiles, value=TRUE)
-meteAvgLogSerFiles = grep('empirSAD', meteAvgFiles, value=TRUE, invert=TRUE)
-meteAvgLogSer = vector('list', length(meteAvgLogSerFiles))
-meteAvgEmpirSAD = vector('list', length(meteAvgEmpirSADFiles))
-names(meteAvgLogSer) = sub('_mete_sar_avgs.csv', '', meteAvgLogSerFiles)
-names(meteAvgEmpirSAD) = sub('_empirSAD_mete_sar_avgs.csv', '', meteAvgEmpirSADFiles)
-for (i in seq_along(meteAvgLogSerFiles)) {
-  meteAvgLogSer[[i]] = read.csv(paste('./sar/', meteAvgLogSerFiles[i], sep=''))
-  meteAvgLogSer[[i]] = meteAvgLogSer[[i]][ , -1]
-  Amin = empir[[match(names(meteAvgLogSer)[i], names(empir))]]$area[1]
-  meteAvgLogSer[[i]]$grains = meteAvgLogSer[[i]]$grains * Amin
-}
-for (i in seq_along(meteAvgEmpirSADFiles)) {
-  meteAvgEmpirSAD[[i]] = read.csv(paste('./sar/', meteAvgEmpirSADFiles[i], sep=''))
-  meteAvgEmpirSAD[[i]] = meteAvgEmpirSAD[[i]][ , -1]
-  Amin = empir[[match(names(meteAvgEmpirSAD)[i], names(empir))]]$area[1]
-  meteAvgEmpirSAD[[i]]$grains = meteAvgEmpirSAD[[i]]$grains * Amin
-}
-
 ## load expected sars under random placement, object is called 'srExp'
 load('./sar/expected_empir_sars.Rdata')
 
-## average cocoli and sherman plots
-sherman = c('sherman1', 'sherman2')
-cocoli = c('cocoli1', 'cocoli2')
-empir = avg_site_results(empir, sherman)
-empir = avg_site_results(empir, cocoli)
-empir = empir[-match('sherman3', names(empir))]
-
-meteLogSer = avg_site_results(meteLogSer, sherman)
-meteLogSer = avg_site_results(meteLogSer, cocoli)
-meteLogSer = meteLogSer[-match('sherman3', names(meteLogSer))]
-
-meteEmpirSAD = avg_site_results(meteEmpirSAD, sherman)
-meteEmpirSAD = avg_site_results(meteEmpirSAD, cocoli)
-meteEmpirSAD = meteEmpirSAD[-match('sherman3', names(meteEmpirSAD))]
-
-meteAvgLogSer = avg_site_results(meteAvgLogSer, sherman)
-meteAvgLogSer = avg_site_results(meteAvgLogSer, cocoli)
-meteAvgLogSer = meteAvgLogSer[-match('sherman3', names(meteAvgLogSer))]
-
-meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, sherman)
-meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, cocoli)
-meteAvgEmpirSAD = meteAvgEmpirSAD[-match('sherman3', names(meteAvgEmpirSAD))]
-
-srExp = avg_site_results(srExp, sherman)
-srExp = avg_site_results(srExp, cocoli)
-srExp = srExp[-match('sherman3', names(srExp))]
+sites = names(empir)
+if (all(as.vector(sapply(c('sherman', 'cocoli'), grepl, sites)))) {
+  ## average cocoli and sherman plots
+  sherman = c('sherman1', 'sherman2')
+  cocoli = c('cocoli1', 'cocoli2')
+  empir = avg_site_results(empir, sherman)
+  empir = avg_site_results(empir, cocoli)
+  empir = empir[-match('sherman3', names(empir))]
+  
+  meteLogSer = avg_site_results(meteLogSer, sherman)
+  meteLogSer = avg_site_results(meteLogSer, cocoli)
+  meteLogSer = meteLogSer[-match('sherman3', names(meteLogSer))]
+  
+  meteEmpirSAD = avg_site_results(meteEmpirSAD, sherman)
+  meteEmpirSAD = avg_site_results(meteEmpirSAD, cocoli)
+  meteEmpirSAD = meteEmpirSAD[-match('sherman3', names(meteEmpirSAD))]
+  
+  meteAvgLogSer = avg_site_results(meteAvgLogSer, sherman)
+  meteAvgLogSer = avg_site_results(meteAvgLogSer, cocoli)
+  meteAvgLogSer = meteAvgLogSer[-match('sherman3', names(meteAvgLogSer))]
+  
+  meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, sherman)
+  meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, cocoli)
+  meteAvgEmpirSAD = meteAvgEmpirSAD[-match('sherman3', names(meteAvgEmpirSAD))]
+  
+  srExp = avg_site_results(srExp, sherman)
+  srExp = avg_site_results(srExp, cocoli)
+  srExp = srExp[-match('sherman3', names(srExp))]
+}
 
 ## compute residuals
-meteLogSer_avg_res = get_sar_resids(empir, meteAvgLogSer, 'richness', 'sr.avg')
 meteLogSer_iter_res = get_sar_resids(empir, meteLogSer, 'richness', 'sr_iter')
 meteLogSer_noniter_res = get_sar_resids(empir, meteLogSer, 'richness', 'sr_noniter')
 
-meteEmpirSAD_avg_res = get_sar_resids(empir, meteAvgEmpirSAD, 'richness', 'sr.avg')
 meteEmpirSAD_iter_res = get_sar_resids(empir, meteEmpirSAD, 'richness', 'sr_iter')
 meteEmpirSAD_noniter_res = get_sar_resids(empir, meteEmpirSAD, 'richness', 'sr_noniter')
 
@@ -95,12 +76,10 @@ rpLogSer_res = get_sar_resids(empir, srExp, 'richness', 'S_logser_binom')
 rpEmpirSAD_res = get_sar_resids(empir, srExp, 'richness', 'S_binom')
 
 
-sar_res = data.frame(site = meteLogSer_avg_res$site, area = meteLogSer_avg_res$area,
-                     logser_avg = meteLogSer_avg_res$res,
+sar_res = data.frame(site = meteLogSer_iter_res$site, area = meteLogSer_iter_res$area,
                      logser_iter = meteLogSer_iter_res$res,
                      logser_noniter = meteLogSer_noniter_res$res,
                      logser_rp = rpLogSer_res$res,
-                     empirsad_avg = meteEmpirSAD_avg_res$res,
                      empirsad_iter = meteEmpirSAD_iter_res$res,
                      empirsad_noniter = meteEmpirSAD_noniter_res$res,
                      empirsad_rp = rpEmpirSAD_res$res)
@@ -119,7 +98,7 @@ habitat = as.character(read.table('./data/habitat.txt', colClasses='character'))
 sar_res$hab = habitat[match(sar_res$site, shrtnm)]
 
 sar_data = sar_res
-sar_data[ , 3:10] = sar_data$richness - sar_data[ , 3:10] 
+sar_data[ , 3:8] = sar_data$richness - sar_data[ , 3:8] 
 
 ## export files
 write.csv(sar_res, file='./sar/sar_residuals.csv', row.names=F)
