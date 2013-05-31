491acf5d0b6f332fc0c5f08b26e2dbf4dd5661bb
Daniel <danmcglinn@gmail.com>
2013-05-31 17:14:27 -0600

    Drop download_data() function.

--- scripts/spat_download_empir_data.R
+++ scripts/spat_download_empir_data.R
@@ -1,28 +1,12 @@
-
 setwd('~/maxent/spat')
+
+dir.create('./data/raw_data')
 
 library(RCurl)
 
-urls = read.csv('./public_data_urls.csv', colClasses='character')
+source('./scripts/spat_functions.R')
 
-download_data = function(urls, delim, output_path) {
-  for (i in seq_along(urls)) {
-    temp = getURL(urls[i])
-    if (delim[i] == 'comma')
-      dat = read.csv(textConnection(temp))
-    if (delim[i] == 'tab')
-      dat = read.delim(textConnection(temp))
-    namesplit = unlist(strsplit(urls[i], split='/'))
-    filename = namesplit[length(namesplit)]
-    if (delim[i] == 'comma')
-      write.csv(dat, file = file.path(output_path, filename), row.names=FALSE)
-    if (delim[i] == 'tab')
-      write.table(dat, file = file.path(output_path, filename), sep='\t',
-                  row.names=FALSE)
-    print(paste(filename, ' was downloaded and written to ',
-                 file.path(output_path, filename), sep=''))
-  }
-}
+urls = read.csv('./public_data_urls.csv', colClasses='character')
 
 print('Downloading datasets, ...')
 
