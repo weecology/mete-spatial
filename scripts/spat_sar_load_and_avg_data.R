c95e0ea4f0515b09389226a58dc98f35545f4af0
Daniel McGlinn <danmcglinn@gmail.com>
2014-01-30 10:20:01 -0700

    Add the averaging of the simulated mete sars back if optional command line argument is provided to the script spat_sar_load_and_avg_data.R

--- scripts/spat_sar_load_and_avg_data.R
+++ scripts/spat_sar_load_and_avg_data.R
@@ -1,8 +1,15 @@
 ## Loads and averages the species area anaylsis data products
 
+
 source('./scripts/spat_functions.R')
 
 print('Aggregating SAR results, ...')
+
+clArgs = commandArgs(trailingOnly=TRUE)
+if (length(clArgs) > 0) {
+  write_files = as.logical(clArgs[1])
+  avg_sim = as.logical(clArgs[2])
+}
 
 fileNames = dir('./sar')
 
@@ -33,6 +40,29 @@ for (i in seq_along(meteEmpirSADFiles)) {
 ## load expected sars under random placement, object is called 'srExp'
 load('./sar/expected_empir_sars.Rdata')
 
+if (avg_sim) {
+  ## mete SARs averaged Simulated
+  meteAvgFiles = grep('mete_sar_avgs.csv', fileNames, value=TRUE)
+  meteAvgEmpirSADFiles = grep('empirSAD', meteAvgFiles, value=TRUE)
+  meteAvgLogSerFiles = grep('empirSAD', meteAvgFiles, value=TRUE, invert=TRUE)
+  meteAvgLogSer = vector('list', length(meteAvgLogSerFiles))
+  meteAvgEmpirSAD = vector('list', length(meteAvgEmpirSADFiles))
+  names(meteAvgLogSer) = sub('_mete_sar_avgs.csv', '', meteAvgLogSerFiles)
+  names(meteAvgEmpirSAD) = sub('_empirSAD_mete_sar_avgs.csv', '', meteAvgEmpirSADFiles)
+  for (i in seq_along(meteAvgLogSerFiles)) {
+    meteAvgLogSer[[i]] = read.csv(paste('./sar/', meteAvgLogSerFiles[i], sep=''))
+    meteAvgLogSer[[i]] = meteAvgLogSer[[i]][ , -1]
+    Amin = empir[[match(names(meteAvgLogSer)[i], names(empir))]]$area[1]
+    meteAvgLogSer[[i]]$grains = meteAvgLogSer[[i]]$grains * Amin
+  }
+  for (i in seq_along(meteAvgEmpirSADFiles)) {
+    meteAvgEmpirSAD[[i]] = read.csv(paste('./sar/', meteAvgEmpirSADFiles[i], sep=''))
+    meteAvgEmpirSAD[[i]] = meteAvgEmpirSAD[[i]][ , -1]
+    Amin = empir[[match(names(meteAvgEmpirSAD)[i], names(empir))]]$area[1]
+    meteAvgEmpirSAD[[i]]$grains = meteAvgEmpirSAD[[i]]$grains * Amin
+  }
+}
+
 sites = names(empir)
 if (any(as.vector(sapply(c('sherman', 'cocoli'), grepl, sites)))) {
   ## average cocoli and sherman plots
@@ -50,13 +80,15 @@ if (any(as.vector(sapply(c('sherman', 'cocoli'), grepl, sites)))) {
   meteEmpirSAD = avg_site_results(meteEmpirSAD, cocoli)
   meteEmpirSAD = meteEmpirSAD[-match('sherman3', names(meteEmpirSAD))]
   
-#  meteAvgLogSer = avg_site_results(meteAvgLogSer, sherman)
-#  meteAvgLogSer = avg_site_results(meteAvgLogSer, cocoli)
-#  meteAvgLogSer = meteAvgLogSer[-match('sherman3', names(meteAvgLogSer))]
-  
-#  meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, sherman)
-#  meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, cocoli)
-#  meteAvgEmpirSAD = meteAvgEmpirSAD[-match('sherman3', names(meteAvgEmpirSAD))]
+  if (avg_sim) {
+    meteAvgLogSer = avg_site_results(meteAvgLogSer, sherman)
+    meteAvgLogSer = avg_site_results(meteAvgLogSer, cocoli)
+    meteAvgLogSer = meteAvgLogSer[-match('sherman3', names(meteAvgLogSer))]
+    
+    meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, sherman)
+    meteAvgEmpirSAD = avg_site_results(meteAvgEmpirSAD, cocoli)
+    meteAvgEmpirSAD = meteAvgEmpirSAD[-match('sherman3', names(meteAvgEmpirSAD))]
+  }
   
   srExp = avg_site_results(srExp, sherman)
   srExp = avg_site_results(srExp, cocoli)
@@ -73,14 +105,32 @@ meteEmpirSAD_noniter_res = get_sar_resids(empir, meteEmpirSAD, 'richness', 'sr_n
 rpLogSer_res = get_sar_resids(empir, srExp, 'richness', 'S_logser_binom')
 rpEmpirSAD_res = get_sar_resids(empir, srExp, 'richness', 'S_binom')
 
+if (avg_sim) {
+  meteLogSer_avg_res = get_sar_resids(empir, meteAvgLogSer, 'richness', 'sr.avg')
+  meteEmpirSAD_avg_res = get_sar_resids(empir, meteAvgEmpirSAD, 'richness', 'sr.avg')
+}
 
-sar_res = data.frame(site = meteLogSer_iter_res$site, area = meteLogSer_iter_res$area,
-                     logser_iter = meteLogSer_iter_res$res,
-                     logser_noniter = meteLogSer_noniter_res$res,
-                     logser_rp = rpLogSer_res$res,
-                     empirsad_iter = meteEmpirSAD_iter_res$res,
-                     empirsad_noniter = meteEmpirSAD_noniter_res$res,
-                     empirsad_rp = rpEmpirSAD_res$res)
+if (avg_sim) {
+  sar_res = data.frame(site = meteLogSer_avg_res$site, area = meteLogSer_iter_res$area,
+                       logser_avg = meteLogSer_avg_res$res,
+                       logser_iter = meteLogSer_iter_res$res,
+                       logser_noniter = meteLogSer_noniter_res$res,
+                       logser_rp = rpLogSer_res$res,
+                       empirsad_avg = meteEmpirSAD_avg_res$res,
+                       empirsad_iter = meteEmpirSAD_iter_res$res,
+                       empirsad_noniter = meteEmpirSAD_noniter_res$res,
+                       empirsad_rp = rpEmpirSAD_res$res)
+}
+
+if (!avg_sim) {
+  sar_res = data.frame(site = meteLogSer_iter_res$site, area = meteLogSer_iter_res$area,
+                       logser_iter = meteLogSer_iter_res$res,
+                       logser_noniter = meteLogSer_noniter_res$res,
+                       logser_rp = rpLogSer_res$res,
+                       empirsad_iter = meteEmpirSAD_iter_res$res,
+                       empirsad_noniter = meteEmpirSAD_noniter_res$res,
+                       empirsad_rp = rpEmpirSAD_res$res) 
+}
 
 ## Add richness and individual density to residual data.frame
 sar_data = read.csv('./sar/empir_sars.csv')
@@ -96,10 +146,16 @@ habitat = as.character(read.table('./data/habitat.txt', colClasses='character'))
 sar_res$hab = habitat[match(sar_res$site, shrtnm)]
 
 sar_data = sar_res
-sar_data[ , 3:8] = sar_data$richness - sar_data[ , 3:8] 
+if (avg_sim) 
+  sar_data[ , 3:10] = sar_data$richness - sar_data[ , 3:10] 
+if (!avg_sim)
+  sar_data[ , 3:8] = sar_data$richness - sar_data[ , 3:8] 
+
 
 ## export files
-write.csv(sar_res, file='./sar/sar_residuals.csv', row.names=F)
-write.csv(sar_data, file='./sar/sar_raw_data.csv', row.names=F)
+if (write_files) {
+  write.csv(sar_res, file='./sar/sar_residuals.csv', row.names=F)
+  write.csv(sar_data, file='./sar/sar_raw_data.csv', row.names=F)
+}
 
 print('Aggregating SAR results, complete!')
