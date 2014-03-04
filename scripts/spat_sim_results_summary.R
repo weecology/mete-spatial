d4eaa0d1ed39776010acab22b4260a1081e9f2d3
Daniel <danmcglinn@gmail.com>
2014-03-04 16:52:11 -0700

    Improve handling of site names, and drop plotting routines.

--- scripts/spat_sim_results_summary.R
+++ scripts/spat_sim_results_summary.R
@@ -1,11 +1,11 @@
 ## Purpose: to graphically summarize the simulated DDR results for each
 ## empirical dataset
+
+print('Aggregating and exporting the simulated DDR results, ...')
+
 source('./scripts/spat_functions.R')
 
-shrtnames = c('bci', 'cocoli1', 'cocoli2', 'cross', 'sherman1', 'sherman2', 
-              'sherman3', 'serp', 'oosting', 'ferp', 'luquillo', 'graveyard',
-              'landsend', 'rocky', 'bormann', 'woodbridge', 'baldmnt', 'bryan',
-              'bigoak')
+shrtnames = as.character(read.table('./data/shrtnames.txt', colClasses='character')[1,])
 
 files = get_file_names(path = './sorensen/', 
                       strings = list(shrtnames, 'C200', 'bisect', 'sherman3'), 
@@ -29,9 +29,8 @@ for (i in seq_along(longnames)) {
   names(simSorAbuAvg[[i]]) = newnames[-(10:12)]
 }
 
-## export as files
-#save(simSorBinAvg, simSorAbuAvg, file = './sorensen/simEmpirSorAvg_bisect.Rdata')
-load('./sorensen/simEmpirSorAvg_bisect.Rdata')
+## export the aggregrated raw results files
+save(simSorBinAvg, simSorAbuAvg, file = './sorensen/simEmpirSorAvg_bisect.Rdata')
 
 ## split results that were generated from log-series and those that were fixed abu
 logser = grep('empirSAD', longnames, invert=TRUE)
@@ -49,135 +48,9 @@ simSorBinFixed = merge_drop(simSorBinFixed )
 simSorAbuLogSer = merge_drop(simSorAbuLogSer)
 simSorAbuFixed = merge_drop(simSorAbuFixed)
 
-## update shrnames
-shrtnames = shrtnames[-match(c('cocoli2','sherman2', 'sherman3'), shrtnames)] 
-
-## export results
+## export the agregated merged results
 save(simSorAbuLogSer, simSorAbuFixed, simSorBinLogSer, simSorBinFixed,
      file = './simulated_empirical_results_bisect.Rdata')
 
-## load the Results
-load('./simulated_empirical_results_bisect.Rdata')
+print('Aggregating and exporting the simulated DDR results, complete!')
 
-
-## examine a single simulated distance decay pattern----------------------------
-tmp = simSorAbuLogSer[3]
-col = colorRampPalette(c('dodgerblue', 'red'))(5)
-range(tmp[[1]]$Avg)
-grains = unique(tmp[[1]]$Comm)
-
-plot(1:10, type='n', xlab='', ylab='', xlim=c(0, 110), 
-     ylim = c(0, 0.4) , frame.plot=F, axes=F, log='')
-axis(side=1, cex.axis=1.25, lwd=2)
-axis(side=2, cex.axis=1.25, lwd=2)
-plotEmpir(tmp, 'average', log='xy', title=F, 
-          quants=F, col=col, lwd=5, add=TRUE, type='p', pch=19)
-for (g in seq_along(grains)) {
-  mod = lm(log(Avg) ~ log(Dist), data=tmp[[1]], subset=Comm == grains[g],
-           weights = N)
-  lines(tmp[[1]]$Dist[tmp[[1]]$Comm == grains[g]], exp(predict(mod)),
-        col=col[g], lwd=4)  
-}
-
-###
-plot(1:10, type='n', xlab='', ylab='', xlim=c(1.5, 120), 
-     ylim = c(0.006, 0.40) , frame.plot=F, axes=F, log='xy')
-axis(side=1, cex.axis=1.75, padj=.5, lwd=8)
-axis(side=2, cex.axis=1.75, lwd=8, at=c(0.01, 0.02, 0.05, 0.10, 0.20, 0.40))
-plotEmpir(tmp, 'average', log='xy', title=F, 
-          quants=F, colcol, lwd=5, add=TRUE, type='p', cex=1.5, pch=19)
-for (g in seq_along(grains)) {
-  mod = lm(log(Avg) ~ log(Dist), data=tmp[[1]], subset=Comm == grains[g],
-           weights = N)
-  lines(tmp[[1]]$Dist[tmp[[1]]$Comm == grains[g]], exp(predict(mod)),
-        col=col[g], lwd=6)  
-}
-##
-plot(log2(Avg) ~ log2(Dist), data=tmp[[1]], type='n', xlab='', ylab='',
-     frame.plot=F, axes=F)
-axis(side=1, cex.axis=1.25, lwd=2)
-axis(side=2, cex.axis=1.25, lwd=2)
-for (g in seq_along(grains)) {
-  mod = lm(log2(Avg) ~ log2(Dist), data=tmp[[1]], subset=Comm == grains[g],
-           weights = N)
-  points(log2(Avg) ~ log2(Dist), data=tmp[[1]], subset=Comm == grains[g],
-         col=col[g], pch=19, lwd=5)
-  lines(log2(tmp[[1]]$Dist[tmp[[1]]$Comm == grains[g]]), predict(mod),
-        col=col[g], lwd=4)  
-}
-
-
-## export pdf summaries of the empirical and simulated results------------------
-mk_summary = function() {
-    par(mfrow=c(2,3))
-    comp_results(site,
-                 list(results1),
-                 list("quants=F, expected=TRUE, sub='RandomPlacement',
-                       metric='average', ylim=ylim, col=col, log=log_tran"))
-    comp_results(site,
-                 list(results1, results2),
-                 list("quants=F, sub='Fixed'",
-                      "quants=T, lwd=2, lty=2"),
-                 metric='average', ylim=ylim, col=col, log=log_tran)
-    comp_results(site,
-                 list(results1, results3),
-                 list("quants=F, sub='LogSer'",
-                      "quants=T, lwd=2, lty=2"),
-                 metric='average', ylim=ylim,col=col, log=log_tran)
-    ###
-    comp_results(site,
-                 list(results1),
-                 list("quants=F, ylim=ylim,
-                       col=col, log=log_tran"))
-    comp_results(site,
-                 list(results1, results2),
-                 list("quants=T, sub='AbuFixed'",
-                      "quants=T, lwd=2, lty=2"),
-                 metric='median', ylim=ylim, col=col, log=log_tran)
-    comp_results(site,
-                 list(results1, results3),
-                 list("quants=T, sub='LogSer'",
-                      "quants=T, lwd=2, lty=2"),
-                 metric='median', ylim=ylim,col=col, log=log_tran)
-}
-
-
-load('./sorensen/empirSorBin_bisect.Rdata')
-load('./sorensen/empirSorAbu_bisect.Rdata')
-
-##------------------------------------------------------------------------------
-results1 = empirSorAbu
-results2 = simSorAbuFixed
-results3 = simSorAbuLogSer
-metric = 'sorensen'
-dataType = 'abu'
-log_tran='xy'
-ylim=list(c(0.1,1))
-pdf(paste('./figs/empir_sim_', metric, '_', dataType, '_bisect_comp.pdf', sep=''),
-    width=7*2, height=7*1.5)
-  for (i in seq_along(results2)) {
-    site = names(results1)[i]
-    col = eval(parse(text=paste('terrain.colors(length(unique(results1$', site,
-                                '$Comm)) + 2)', sep='')))
-    mk_summary()
-  }
-dev.off()
-##------------------------------------------------------------------------------
-
-results1 = empirSorBin
-results2 = simSorBinFixed
-results3 = simSorBinLogSer
-metric = 'sorensen'
-dataType = 'bin'
-log_tran='xy'
-ylim=list(c(0.1,1))
-pdf(paste('./figs/empir_sim_', metric, '_', dataType, '_bisect_comp.pdf', sep=''),
-    width=7*2, height=7*1.5)
-  for (i in seq_along(results2)) {
-    site = names(results1)[i]
-    col = eval(parse(text=paste('terrain.colors(length(unique(results1$', site,
-                                '$Comm)) + 2)', sep='')))
-    mk_summary()
-  }
-dev.off()
-##------------------------------------------------------------------------------
