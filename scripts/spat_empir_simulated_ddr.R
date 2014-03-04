71d63aa5be4477e3b36c80f8841711f263b3fff4
Daniel <danmcglinn@gmail.com>
2014-03-04 16:53:58 -0700

    Allow specification of the number of communities that were simulated so correct community file is imported. Fix relative paths. Add print statements.

--- scripts/spat_empir_simulated_ddr.R
+++ scripts/spat_empir_simulated_ddr.R
@@ -1,13 +1,16 @@
 ## Purpose: to compute the empirical simulated DDR 
 
+print('Submitting jobs to compute the METE simulated DDRs, ...')
+
 clArgs = commandArgs(trailingOnly=TRUE)
 
 server = clArgs[1]
 commName = clArgs[2]
-dataType = clArgs[3]
-bisect = clArgs[4]
-memory = as.numeric(clArgs[5])
-iteration = as.numeric(clArgs[6])
+ncomm = clArgs[3]
+dataType = clArgs[4]
+bisect = clArgs[5]
+memory = as.numeric(clArgs[6])
+iteration = as.numeric(clArgs[7])
 
 if (dataType == 'both') {
   dataType = c("abu", "binary")
@@ -20,12 +23,12 @@ if (is.na(iteration)) {
 }
 
 ## arguments for job
-S = read.table('../data/S_vals.txt')
-N = read.table('../data/N_vals.txt')
-bisect_fine = read.table('../data/bisect_fine.txt')
-bisect_coarse = read.table('../data/bisect_coarse.txt')
-grain_fine = read.table('../data/grain_fine.txt')
-names = as.character(read.table('../data/shrtnames.txt', colClasses='character'))
+S = read.table('./data/S_vals.txt')
+N = read.table('./data/N_vals.txt')
+bisect_fine = read.table('./data/bisect_fine.txt')
+bisect_coarse = read.table('./data/bisect_coarse.txt')
+grain_fine = read.table('./data/grain_fine.txt')
+names = as.character(read.table('./data/shrtnames.txt', colClasses='character'))
 
 commName = unlist(strsplit(commName, ' '))
 if (commName[1] == 'all') {
@@ -40,7 +43,7 @@ for (i in indices) {
   for (j in sadType) {
     for (k in dataType) {
       for (m in metrics) {
-        log_file = paste('./log_files/error_sim_analysis_', names[i],
+        log_file = paste('./scripts/log_files/error_sim_analysis_', names[i],
                          '_', j, '.log', sep='')
         if (j == 'meteSAD')
           name = names[i]
@@ -48,17 +51,19 @@ for (i in indices) {
           name = paste(names[i], '_empirSAD', sep='')
         if ( server == 'unc') 
           system(paste('bsub -q week -M', memory, '-J', names[i],
-                       '-o', log_file, 'Rscript spat_analysis.R',
-                       S[i], N[i], 200, bisect_fine[i], bisect_coarse[i],
+                       '-o', log_file, 'Rscript ./scripts/spat_analysis.R',
+                       S[i], N[i], ncomm, bisect_fine[i], bisect_coarse[i],
                        grain_fine[i], FALSE, k, m, bisect, NA, NA,
                        name, TRUE, iteration, sep=' '))
         else
-          system(paste('nice Rscript spat_analysis.R', S[i], N[i], 200,
+          system(paste('nice Rscript ./scripts/spat_analysis.R', S[i], N[i], ncomm,
                        bisect_fine[i], bisect_coarse[i],
                        grain_fine[i], FALSE, k, m, bisect, NA, NA,
-                       name, TRUE, iteration, '>', log_file, '2>&1 &', sep=' '),
+                       name, TRUE, iteration, '>', log_file, '2>&1', sep=' '),
                  wait=FALSE)
       }    
     }
   }
 }
+
+print('Submitting jobs to compute the METE simulated DDRs, complete!')
